32481893e1ba9631ab6b1cfaf5a1ffe66546b7a6
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-05-27 17:09:47 +0300

    Export source

--- Finish.py
+++ Finish.py
@@ -4,6 +4,16 @@ shutil.copyfile('Source/ModernGL.h', 'Bin/ModernGL.h')
 shutil.copyfile('Source/ModernGL.h', 'Bin/ModernGL')
 shutil.copyfile('Temp/libOpenGL.a', 'Bin/libOpenGL.a')
 
+if not os.path.isdir('Bin/Source'):
+	os.mkdir('Bin/Source')
+
+shutil.copyfile('Source/ModernGL.h', 'Bin/Source/ModernGL.h')
+shutil.copyfile('Source/ModernGL.cpp', 'Bin/Source/ModernGL.cpp')
+shutil.copyfile('libOpenGL.a/Bin/WindowsGL.h', 'Bin/Source/WindowsGL.h')
+shutil.copyfile('libOpenGL.a/Bin/WindowsGL.cpp', 'Bin/Source/WindowsGL.cpp')
+shutil.copyfile('libOpenGL.a/Bin/OpenGL.h', 'Bin/Source/OpenGL.h')
+shutil.copyfile('libOpenGL.a/Bin/OpenGL.cpp', 'Bin/Source/OpenGL.cpp')
+
 import zipfile
 
 packet = zipfile.ZipFile('Bin/Release.zip', 'w')
