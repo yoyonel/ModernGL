4bacf2b6bc11705cb7c0e4f05b03acca7ab6001b
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-21 20:39:35 +0300

    2.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ ModernGL = Extension(
 
 setup(
 	name = 'ModernGL',
-	version = '2.0.1',
+	version = '2.0.2',
 	description = 'ModernGL',
 	url = 'https://github.com/cprogrammer1994/ModernGL',
 	author = 'Szabolcs Dombi',
