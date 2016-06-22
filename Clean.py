1a9835be7c8733fc5ce83820833fc98b21438f5c
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-22 13:52:26 +0300

    Clean

---
+++ Clean.py
@@ -0,0 +1,16 @@
+import shutil, os
+
+if os.path.isdir('Bin'):
+	shutil.rmtree('Bin')
+	
+if os.path.isdir('Temp'):
+	shutil.rmtree('Temp')
+
+if os.path.isdir('Build'):
+	shutil.rmtree('Build')
+
+if os.path.isdir('Dist'):
+	shutil.rmtree('Dist')
+
+if os.path.isdir('ModernGL.egg-info'):
+	shutil.rmtree('ModernGL.egg-info')
