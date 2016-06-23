1d2ae56e38f084457c6f7bc633e66d01fad83c50
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-23 11:57:12 +0300

    VERSION public

--- Source/Python-ModernGL.cpp
+++ Source/Python-ModernGL.cpp
@@ -2052,7 +2052,8 @@ PyObject * PyInit_ModernGL() {
 	PyModule_AddIntConstant(m, "ENABLE_DEPTH_TEST", 0x04);
 	PyModule_AddIntConstant(m, "ENABLE_MULTISAMPLE", 0x08);
 
-	PyModule_AddStringConstant(m, "__MODULE_VERSION__", "2.0.4");
+	PyModule_AddStringConstant(m, "VERSION", "2.0.4");
+	
 	PyModule_AddStringConstant(m, "__AUTHOR_NAME__", "Szabolcs Dombi");
 	PyModule_AddStringConstant(m, "__AUTHOR_EMAIL__", "cprogrammer1994@gmail.com");
 
