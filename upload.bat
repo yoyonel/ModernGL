26dd8fb6c4151df45b3f9cc60fb7d3dbb2f17a92
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-23 11:16:59 +0300

    merge sdist + bdist

---
+++ upload.bat
@@ -0,0 +1,5 @@
+call python setup.py sdist upload
+call python setup.py bdist upload
+call python setup.py bdist_wheel upload
+call python64 setup.py bdist upload
+call python64 setup.py bdist_wheel upload
