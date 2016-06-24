62ca6d31aa6188ebff67021b7d3bcca40787e3c5
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-24 10:51:52 +0300

    newlines

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, Extension
 
 ShortDescription = 'Wrapper over OpenGL'
 
-LongDescription = '''OpenGL is a great environment for developing portable, platform independent,
+LongDescription = '''
+OpenGL is a great environment for developing portable, platform independent,
 interactive 2D and 3D graphics applications. The API implementation in Python
 is cumbersome, resulting in applications with high latency. To solve this
 problem we have developed ModernGL, a wrapper over OpenGL that simplifies the
@@ -10,7 +11,8 @@ creation of simple graphics applications like scientific simulations, small
 games or user interfaces. Usually, acquiring in-depth knowledge of OpenGL
 requires a steep learning curve. In contrast, ModernGL is easy to learn and
 use, moreover it is capable of rendering with the same performance and
-quality, with less code written.'''
+quality, with less code written.
+'''
 
 Classifiers = [
 	'Development Status :: 2 - Pre-Alpha',
