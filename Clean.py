0196c38fa4e0a31cec7832e1dafb47e2b8820c65
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-06-26 20:34:42 +0300

    --cache

--- Clean.py
+++ Clean.py
@@ -2,7 +2,7 @@ import shutil, os
 
 if os.path.isdir('Bin'):
 	shutil.rmtree('Bin')
-	
+
 if os.path.isdir('Temp'):
 	shutil.rmtree('Temp')
 
@@ -11,6 +11,9 @@ if os.path.isdir('Build'):
 
 if os.path.isdir('Dist'):
 	shutil.rmtree('Dist')
+
+if os.path.isdir('ModernGL/__pycache__'):
+	shutil.rmtree('ModernGL/__pycache__')
 
 if os.path.isdir('ModernGL.egg-info'):
 	shutil.rmtree('ModernGL.egg-info')
