2fa42cbcde845b321fcc4e374116c407f7f32027
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-07-06 00:08:11 +0300

    +

--- multi_upload.bat
+++ multi_upload.bat
@@ -1,6 +1,6 @@
-python Clean.py
+rem python Clean.py
 SET CUSTOM_GCC=YES
-call pythons.bat setup.py install
+rem call pythons.bat setup.py install
 call pythons setup.py bdist_wheel upload
 call pythons setup.py bdist_egg upload
 call pythons setup.py bdist_wininst upload
