b6b238dcefc1db60d0d1741606619e6cd3a79362
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-07-05 21:47:29 +0300

    read imp #8 + checks

--- Source/ModernGL-Framebuffer.cpp
+++ Source/ModernGL-Framebuffer.cpp
@@ -100,10 +100,9 @@ PyObject * ReadPixels(PyObject * self, PyObject * args, PyObject * kwargs) {
 		return 0;
 	}
 
-	if (components < 1 || components > 4) {
-		PyErr_Format(ModuleError, "ReadPixels() argument `components` must be in range 1 to 4, not %d", components);
-		return 0;
-	}
+	CHECK_AND_REPORT_ARG_VALUE_ERROR(width < 1, "width", width);
+	CHECK_AND_REPORT_ARG_VALUE_ERROR(height < 1, "height", height);
+	CHECK_AND_REPORT_ARG_VALUE_ERROR(components < 1 || components > 4, "components", components);
 
 	int size = height * ((width * components + 3) & ~3);
 
@@ -120,53 +119,59 @@ PyObject * ReadPixels(PyObject * self, PyObject * args, PyObject * kwargs) {
 }
 
 PyObject * ReadDepthPixels(PyObject * self, PyObject * args, PyObject * kwargs) {
-	PyErr_SetString(ModuleError, "No Imp.");
-	return 0;
-	// int x;
-	// int y;
-	// int width;
-	// int height;
+	int x;
+	int y;
+	int width;
+	int height;
 
-	// static const char * kwlist[] = {"x", "y", "width", "height", 0};
+	static const char * kwlist[] = {"x", "y", "width", "height", 0};
 
-	// if (!PyArg_ParseTupleAndKeywords(args, kwargs, "iiii:ReadDepthPixels", (char **)kwlist, &x, &y, &width, &height)) {
-	// 	return 0;
-	// }
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "iiii:ReadDepthPixels", (char **)kwlist, &x, &y, &width, &height)) {
+		return 0;
+	}
 
-	// int size = height * height * 4;
-	// float * pixels = ModernGL::ReadDepthPixels(x, y, width, height);
-	// PyObject * data = PyBytes_FromStringAndSize((const char *)pixels, size);
-	// free(pixels);
+	CHECK_AND_REPORT_ARG_VALUE_ERROR(width < 1, "width", width);
+	CHECK_AND_REPORT_ARG_VALUE_ERROR(height < 1, "height", height);
 
-	// return data;
+	int size = height * height * 4;
+	float * pixels = ModernGL::ReadDepthPixels(x, y, width, height);
+	PyObject * data = PyBytes_FromStringAndSize((const char *)pixels, size);
+	free(pixels);
+
+	PyObject * bytes = PyBytes_FromStringAndSize(0, size);
+	char * data = PyBytes_AS_STRING(bytes);
+	OpenGL::glReadPixels(x, y, width, height, OpenGL::GL_DEPTH_COMPONENT, OpenGL::GL_FLOAT, data);
+	data[size] = 0;
+
+	return bytes;
 }
 
 PyObject * ReadPixel(PyObject * self, PyObject * args, PyObject * kwargs) {
-	PyErr_SetString(ModuleError, "No Imp.");
-	return 0;
-	// int x;
-	// int y;
+	int x;
+	int y;
 
-	// static const char * kwlist[] = {"x", "y", 0};
+	static const char * kwlist[] = {"x", "y", 0};
 
-	// if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii:ReadPixel", (char **)kwlist, &x, &y)) {
-	// 	return 0;
-	// }
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii:ReadPixel", (char **)kwlist, &x, &y)) {
+		return 0;
+	}
 
-	// return PyLong_FromUnsignedLong(ModernGL::ReadPixel(x, y));
+	unsigned rgba = 0;
+	OpenGL::glReadPixels(x, y, 1, 1, OpenGL::GL_RGBA, OpenGL::GL_UNSIGNED_BYTE, &rgba);
+	return PyLong_FromUnsignedLong(rgba);
 }
 
 PyObject * ReadDepthPixel(PyObject * self, PyObject * args, PyObject * kwargs) {
-	PyErr_SetString(ModuleError, "No Imp.");
-	return 0;
-	// int x;
-	// int y;
+	int x;
+	int y;
 
-	// static const char * kwlist[] = {"x", "y", 0};
+	static const char * kwlist[] = {"x", "y", 0};
 
-	// if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii:ReadDepthPixel", (char **)kwlist, &x, &y)) {
-	// 	return 0;
-	// }
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii:ReadDepthPixel", (char **)kwlist, &x, &y)) {
+		return 0;
+	}
 
-	// return PyFloat_FromDouble(ModernGL::ReadDepthPixel(x, y));
+	float depth = 0.0;
+	OpenGL::glReadPixels(x, y, 1, 1, OpenGL::GL_DEPTH_COMPONENT, OpenGL::GL_FLOAT, &depth);
+	return PyFloat_FromDouble(depth);
 }
