e046a5c07f47ecc8f0dc55b3aa75491ee46d6177
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-07-06 19:06:52 +0300

    description += badges

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ except ImportError:
 ShortDescription = 'modern OpenGL binding for Python'
 
 LongDescription = '''
+|Chat|
+
+|PyPI-1| |PyPI-2| |PyPI-3| |github|
+
+|PyPI-4| |PyPI-5|
+
 OpenGL is a great environment for developing portable, platform independent,
 interactive 2D and 3D graphics applications. The API implementation in Python
 is cumbersome, resulting in applications with high latency. To solve this
@@ -22,6 +28,14 @@ games or user interfaces. Usually, acquiring in-depth knowledge of OpenGL
 requires a steep learning curve. In contrast, ModernGL is easy to learn and
 use, moreover it is capable of rendering with the same performance and
 quality, with less code written.
+
+.. |Chat| image:: https://badges.gitter.im/cprogrammer1994/ModernGL.svg
+.. |PyPI-1| image:: https://img.shields.io/pypi/l/ModernGL.svg
+.. |PyPI-2| image:: https://img.shields.io/pypi/pyversions/ModernGL.svg
+.. |PyPI-3| image:: https://img.shields.io/pypi/v/ModernGL.svg
+.. |github| image:: https://img.shields.io/github/tag/cprogrammer1994/ModernGL.svg
+.. |PyPI-4| image:: https://img.shields.io/pypi/status/ModernGL.svg
+.. |PyPI-5| image:: https://img.shields.io/pypi/dm/ModernGL.svg
 '''
 
 Classifiers = [
