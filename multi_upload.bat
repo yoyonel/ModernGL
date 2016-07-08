ea5f25ac323517df9bb948372920d2a4d767f5d9
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-07-08 13:11:40 +0300

    fix batch

--- multi_upload.bat
+++ multi_upload.bat
@@ -4,3 +4,4 @@ rem call pythons.bat setup.py install
 call pythons setup.py bdist_wheel upload
 call pythons setup.py bdist_egg upload
 call pythons setup.py bdist_wininst upload
+call python setup.py sdist upload
--- upload.bat
+++
@@ -1,9 +0,0 @@
-call python setup.py sdist upload
-
-call python setup.py bdist_wheel upload
-call python setup.py bdist_egg upload
-call python setup.py bdist_wininst upload
-
-call python64 setup.py bdist_wheel upload
-call python64 setup.py bdist_egg upload
-call python64 setup.py bdist_wininst upload
