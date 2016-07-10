e1cd3bd49ba1119f08ac52754c478feeb9fed216
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-07-10 22:39:55 +0300

    changes

--- Clean.py
+++ Clean.py
@@ -1,16 +1,10 @@
 import shutil, os
 
-if os.path.isdir('Bin'):
-	shutil.rmtree('Bin')
+if os.path.isdir('build'):
+	shutil.rmtree('build')
 
-if os.path.isdir('Temp'):
-	shutil.rmtree('Temp')
-
-if os.path.isdir('Build'):
-	shutil.rmtree('Build')
-
-if os.path.isdir('Dist'):
-	shutil.rmtree('Dist')
+if os.path.isdir('dist'):
+	shutil.rmtree('dist')
 
 if os.path.isdir('__pycache__'):
 	shutil.rmtree('__pycache__')
@@ -18,11 +12,14 @@ if os.path.isdir('__pycache__'):
 if os.path.isdir('ModernGL/__pycache__'):
 	shutil.rmtree('ModernGL/__pycache__')
 
+if os.path.isdir('External/__pycache__'):
+	shutil.rmtree('External/__pycache__')
+
+if os.path.isfile('External/cccompiler.pyc'):
+	os.remove('External/cccompiler.pyc')
+
 if os.path.isdir('ModernGL.egg-info'):
 	shutil.rmtree('ModernGL.egg-info')
-
-if os.path.isfile('cccompiler.pyc'):
-	os.remove('cccompiler.pyc')
 
 if os.path.isfile('files.txt'):
 	os.remove('files.txt')
