024b7e24416fef16329cb37a67afefd758549213
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-03-01 21:18:57 +0100

    1-4 Examples

--- examples/GLWindow/02_UniformsAndAttributes.py
+++ examples/GLWindow/02_UniformsAndAttributes.py
@@ -3,40 +3,51 @@ import struct
 
 GLWindow.Init()
 
+# Context
+
 ctx = ModernGL.create_context()
 
-vert = ctx.VertexShader('''
-	#version 330
+# Shaders & Program
 
-	in vec2 vert;
+prog = ctx.Program([
+	ctx.VertexShader('''
+		#version 330
 
-	in vec3 vert_color;
-	out vec3 frag_color;
+		in vec2 vert;
 
-	uniform vec2 scale;
-	uniform float rotation;
+		in vec3 vert_color;
+		out vec3 frag_color;
 
-	void main() {
-		frag_color = vert_color;
-		mat2 rot = mat2(cos(rotation), sin(rotation), -sin(rotation), cos(rotation));
-		gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
-	}
-''')
+		uniform vec2 scale;
+		uniform float rotation;
 
-frag = ctx.FragmentShader('''
-	#version 330
+		void main() {
+			frag_color = vert_color;
+			mat2 rot = mat2(cos(rotation), sin(rotation), -sin(rotation), cos(rotation));
+			gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
+		}
+	'''),
+	ctx.FragmentShader('''
+		#version 330
 
-	in vec3 frag_color;
-	out vec4 color;
+		in vec3 frag_color;
+		out vec4 color;
 
-	void main() {
-		color = vec4(frag_color, 1.0);
-	}
-''')
+		void main() {
+			color = vec4(frag_color, 1.0);
+		}
+	'''),
+])
+
+# Uniforms
+
+scale = prog.uniforms['scale']
+rotation = prog.uniforms['rotation']
 
 width, height = GLWindow.GetSize()
+scale.value = (height / width * 0.75, 0.75)
 
-prog = ctx.Program([vert, frag])
+# Buffer
 
 vbo = ctx.Buffer(struct.pack('15f',
 	1.0, 0.0,
@@ -46,19 +57,22 @@ vbo = ctx.Buffer(struct.pack('15f',
 	0.0, 1.0, 0.0,
 
 	-0.5, -0.86,
-	0.0, 0.0, 1.0
+	0.0, 0.0, 1.0,
 ))
 
-vao = ctx.VertexArray(prog, [
-	(vbo, '2f3f', ['vert', 'vert_color'])
-])
+# Put everything together
 
-scale = prog.uniforms['scale']
-rotation = prog.uniforms['rotation']
+vao = ctx.SimpleVertexArray(prog, vbo, '2f3f', ['vert', 'vert_color'])
 
-scale.value = (height / width * 0.75, 0.75)
+# Main loop
 
 while GLWindow.Update():
 	ctx.clear(240, 240, 240)
 	rotation.value = GLWindow.GetTime()
-	vao.render(ModernGL.TRIANGLES)
+	vao.render()
+
+# Cleanup
+
+vao.release()
+vbo.release()
+prog.release()
--- examples/GLWindow/04_Texture.py
+++ examples/GLWindow/04_Texture.py
@@ -4,56 +4,77 @@ import struct
 
 GLWindow.Init()
 
+# Context
+
 ctx = ModernGL.create_context()
 
-print(ctx.max_texture_units)
+# Shaders & Program
 
-vert = ctx.VertexShader('''
-	#version 330
+prog = ctx.Program([
+	ctx.VertexShader('''
+		#version 330
 
-	in vec2 vert;
-	out vec2 tex_coord;
+		in vec2 vert;
+		out vec2 tex_coord;
 
-	uniform vec2 scale;
-	uniform float rotation;
+		uniform vec2 scale;
+		uniform float rotation;
 
-	void main() {
-		mat2 rot = mat2(cos(rotation), sin(rotation), -sin(rotation), cos(rotation));
-		gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
-		tex_coord = vert;
-	}
-''')
+		void main() {
+			mat2 rot = mat2(cos(rotation), sin(rotation), -sin(rotation), cos(rotation));
+			gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
+			tex_coord = vert;
+		}
+	'''),
+	ctx.FragmentShader('''
+		#version 330
 
-frag = ctx.FragmentShader('''
-	#version 330
+		uniform sampler2D texture;
 
-	uniform sampler2D texture;
+		in vec2 tex_coord;
+		out vec4 color;
 
-	in vec2 tex_coord;
-	out vec4 color;
+		void main() {
+			color = vec4(texture2D(texture, tex_coord).rgb, 1.0);
+		}
+	'''),
+])
 
-	void main() {
-		color = vec4(texture2D(texture, tex_coord).rgb, 1.0);
-	}
-''')
-
-width, height = GLWindow.GetSize()
-
-prog = ctx.Program([vert, frag])
-
-vbo = ctx.Buffer(struct.pack('6f', 1.0, 0.0, -0.5, 0.86, -0.5, -0.86))
-vao = ctx.VertexArray(prog, [(vbo, '2f', ['vert'])])
+# Uniforms
 
 scale = prog.uniforms['scale']
 rotation = prog.uniforms['rotation']
 
+width, height = GLWindow.GetSize()
 scale.value = (height / width * 0.75, 0.75)
+
+# Buffer
+
+vbo = ctx.Buffer(struct.pack('6f',
+	1.0, 0.0,
+	-0.5, 0.86,
+	-0.5, -0.86,
+))
+
+# Put everything together
+
+vao = ctx.SimpleVertexArray(prog, vbo, '2f', ['vert'])
+
+# Texture
 
 img = Image.open('../DataFiles/Noise.jpg')
 texture = ctx.Texture(img.size, 3, img.tobytes())
 texture.use()
 
+# Main loop
+
 while GLWindow.Update():
 	ctx.clear(240, 240, 240)
 	rotation.value = GLWindow.GetTime()
-	vao.render(ModernGL.TRIANGLES)
+	vao.render()
+
+# Cleanup
+
+vao.release()
+vbo.release()
+prog.release()
