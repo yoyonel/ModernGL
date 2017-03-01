e2a3dde6c6ef8db42d6eb4e0c261666396440011
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-03-01 21:18:57 +0100

    1-4 Examples

--- examples/GLWindow/01_HelloWorld.py
+++ examples/GLWindow/01_HelloWorld.py
@@ -3,18 +3,20 @@ import struct
 
 GLWindow.Init()
 
+# Context
+
 ctx = ModernGL.create_context()
+
+# Shaders & Program
 
 prog = ctx.Program([
 	ctx.VertexShader('''
 		#version 330
 
-		uniform vec2 asd;
-
 		in vec2 vert;
 
 		void main() {
-			gl_Position = vec4(asd + vert, 0.0, 1.0);
+			gl_Position = vec4(vert, 0.0, 1.0);
 		}
 	'''),
 	ctx.FragmentShader('''
@@ -28,17 +30,25 @@ prog = ctx.Program([
 	'''),
 ])
 
+# Buffer
+
 vbo = ctx.Buffer(struct.pack('6f',
 	0.0, 0.8,
 	-0.6, -0.8,
-	0.6, -0.8
+	0.6, -0.8,
 ))
+
+# Put everything together
 
 vao = ctx.SimpleVertexArray(prog, vbo, '2f', ['vert'])
 
+# Main loop
+
 while GLWindow.Update():
 	ctx.clear(240, 240, 240)
-	vao.render(ModernGL.TRIANGLES)
+	vao.render()
+
+# Cleanup
 
 vao.release()
 vbo.release()
--- examples/GLWindow/03_Blending.py
+++ examples/GLWindow/03_Blending.py
@@ -3,43 +3,54 @@ import struct
 
 GLWindow.Init()
 
+# Context
+
 ctx = ModernGL.create_context()
 
-vert = ctx.VertexShader('''
-	#version 330
+# Shaders & Program
 
-	in vec2 vert;
+prog = ctx.Program([
+	ctx.VertexShader('''
+		#version 330
 
-	in vec4 vert_color;
-	out vec4 frag_color;
+		in vec2 vert;
 
-	uniform vec2 scale;
-	uniform float rotation;
+		in vec4 vert_color;
+		out vec4 frag_color;
 
-	void main() {
-		frag_color = vert_color;
-		float r = rotation * (0.5 + gl_InstanceID * 0.05);
-		mat2 rot = mat2(cos(r), sin(r), -sin(r), cos(r));
-		gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
-	}
-''')
+		uniform vec2 scale;
+		uniform float rotation;
 
-frag = ctx.FragmentShader('''
-	#version 330
+		void main() {
+			frag_color = vert_color;
+			float r = rotation * (0.5 + gl_InstanceID * 0.05);
+			mat2 rot = mat2(cos(r), sin(r), -sin(r), cos(r));
+			gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
+		}
+	'''),
+	ctx.FragmentShader('''
+		#version 330
 
-	in vec4 frag_color;
-	out vec4 color;
+		in vec4 frag_color;
+		out vec4 color;
 
-	void main() {
-		color = frag_color;
-	}
-''')
+		void main() {
+			color = vec4(frag_color);
+		}
+	'''),
+])
+
+# Uniforms
+
+scale = prog.uniforms['scale']
+rotation = prog.uniforms['rotation']
 
 width, height = GLWindow.GetSize()
+scale.value = (height / width * 0.75, 0.75)
 
-prog = ctx.Program([vert, frag])
+# Buffer
 
-verts = [
+vbo = ctx.Buffer(struct.pack('18f',
 	1.0, 0.0,
 	1.0, 0.0, 0.0, 0.5,
 
@@ -48,20 +59,22 @@ verts = [
 
 	-0.5, -0.86,
 	0.0, 0.0, 1.0, 0.5,
-]
+))
 
-vbo = ctx.Buffer(struct.pack('18f', *verts))
-vao = ctx.VertexArray(prog, [
-	(vbo, '2f4f', ['vert', 'vert_color']),
-])
+# Put everything together
 
-scale = prog.uniforms['scale']
-rotation = prog.uniforms['rotation']
+vao = ctx.SimpleVertexArray(prog, vbo, '2f4f', ['vert', 'vert_color'])
 
-scale.value = (height / width * 0.75, 0.75)
+# Main loop
 
 while GLWindow.Update():
 	ctx.clear(240, 240, 240)
 	ctx.enable(ModernGL.BLEND)
 	rotation.value = GLWindow.GetTime()
-	vao.render(ModernGL.TRIANGLES, instances = 10)
+	vao.render(instances = 10)
+
+# Cleanup
+
+vao.release()
+vbo.release()
+prog.release()
