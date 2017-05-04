5c3a71143f9ccf58813bf714008694e78552c83c
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-04 07:57:04 +0200

    update texture

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -35,6 +35,115 @@ PyObject * MGLTexture_tp_str(MGLTexture * self) {
 	return PyUnicode_FromFormat("<ModernGL.Texture>");
 }
 
+PyObject * MGLTexture_update(MGLTexture * self, PyObject * args, PyObject * kwargs) {
+	static const char * kwlist[] = {"data", "size", "offset", 0};
+
+	PyObject * data;
+	PyObject * size = Py_None;
+	PyObject * offset = Py_None;
+
+	int args_ok = PyArg_ParseTupleAndKeywords(
+		args,
+		kwargs,
+		"O|OO",
+		(char **)kwlist,
+		&data,
+		&size,
+		&offset
+	);
+
+	if (!args_ok) {
+		return 0;
+	}
+
+	int x;
+	int y;
+
+	int width;
+	int height;
+
+	Py_buffer buffer_view;
+
+	if (size != Py_None) {
+		if (Py_TYPE(size) != &PyTuple_Type) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+
+		if (PyTuple_GET_SIZE(size) != 2) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+
+		width = PyLong_AsLong(PyTuple_GET_ITEM(size, 0));
+		height = PyLong_AsLong(PyTuple_GET_ITEM(size, 1));
+
+		if (PyErr_Occurred()) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+	} else {
+		width = self->width;
+		height = self->height;
+	}
+
+	if (offset != Py_None) {
+		if (Py_TYPE(offset) != &PyTuple_Type) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+
+		if (PyTuple_GET_SIZE(offset) != 2) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+
+		x = PyLong_AsLong(PyTuple_GET_ITEM(offset, 0));
+		y = PyLong_AsLong(PyTuple_GET_ITEM(offset, 1));
+
+		if (PyErr_Occurred()) {
+			PyErr_Format(PyExc_Exception, "Unknown error in %s (%s:%d)", __FUNCTION__, __FILE__, __LINE__);
+			return 0;
+		}
+	} else {
+		x = 0;
+		y = 0;
+	}
+
+	int expected_size = self->floats ? (width * height * self->components * 4) : (height * ((width * self->components + 3) & ~3));
+
+	PyObject_GetBuffer(data, &buffer_view, PyBUF_SIMPLE);
+
+	if (buffer_view.len != expected_size) {
+		MGLError * error = MGLError_New(TRACE, "data size mismatch %d != %d", buffer_view.len, expected_size);
+		PyErr_SetObject((PyObject *)&MGLError_Type, (PyObject *)error);
+		if (data != Py_None) {
+			PyBuffer_Release(&buffer_view);
+		}
+		return 0;
+	}
+
+	const int formats[] = {0, GL_RED, GL_RG, GL_RGB, GL_RGBA};
+
+	int pixel_type = self->floats ? GL_FLOAT : GL_UNSIGNED_BYTE;
+	int format = formats[self->components];
+
+	const GLMethods & gl = self->context->gl;
+
+	gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+	gl.BindTexture(GL_TEXTURE_2D, self->texture_obj);
+
+	gl.TexSubImage2D(GL_TEXTURE_2D, 0, x, y, width, height, format, pixel_type, buffer_view.buf);
+
+	PyBuffer_Release(&buffer_view);
+
+	Py_RETURN_NONE;
+}
+
+const char * MGLTexture_update_doc = R"(
+	update()
+)";
+
 PyObject * MGLTexture_use(MGLTexture * self, PyObject * args, PyObject * kwargs) {
 	static const char * kwlist[] = {"index", 0};
 
@@ -83,6 +192,7 @@ const char * MGLTexture_release_doc = R"(
 )";
 
 PyMethodDef MGLTexture_tp_methods[] = {
+	{"update", (PyCFunction)MGLTexture_update, METH_VARARGS | METH_KEYWORDS, MGLTexture_update_doc},
 	{"use", (PyCFunction)MGLTexture_use, METH_VARARGS | METH_KEYWORDS, MGLTexture_use_doc},
 	{"release", (PyCFunction)MGLTexture_release, METH_NOARGS, MGLTexture_release_doc},
 	{0},
