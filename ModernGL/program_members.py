6fcc8b7d8b328bbb9b58caf55d5ffcbed3baa00a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-14 13:33:59 +0200

    move docs

--- ModernGL/program_members.py
+++ ModernGL/program_members.py
@@ -195,7 +195,9 @@ class Attribute:
     @property
     def name(self):
         '''
-            name
+            The attribute name.
+            The name will be filtered to have no array syntax on it's end.
+            Attribute name without ``'[0]'`` ending if any.
         '''
 
         return self.mglo.name
@@ -203,7 +205,8 @@ class Attribute:
     @property
     def location(self):
         '''
-            location
+            The attribute location.
+            The result of the glGetAttribLocation.
         '''
 
         return self.mglo.location
@@ -211,7 +214,7 @@ class Attribute:
     @property
     def array_length(self):
         '''
-            array_length
+            If the attribute is an array the array_length is the length of the array otherwise it is ``1``.
         '''
 
         return self.mglo.array_length
@@ -219,7 +222,79 @@ class Attribute:
     @property
     def dimension(self):
         '''
-            dimension
+            The attribute dimension.
+
+            +-----------+-----------+
+            | GLSL type | dimension |
+            +===========+===========+
+            | int       | 1         |
+            +-----------+-----------+
+            | ivec2     | 2         |
+            +-----------+-----------+
+            | ivec3     | 3         |
+            +-----------+-----------+
+            | ivec4     | 4         |
+            +-----------+-----------+
+            | uint      | 1         |
+            +-----------+-----------+
+            | uvec2     | 2         |
+            +-----------+-----------+
+            | uvec3     | 3         |
+            +-----------+-----------+
+            | uvec4     | 4         |
+            +-----------+-----------+
+            | float     | 1         |
+            +-----------+-----------+
+            | vec2      | 2         |
+            +-----------+-----------+
+            | vec3      | 3         |
+            +-----------+-----------+
+            | vec4      | 4         |
+            +-----------+-----------+
+            | double    | 1         |
+            +-----------+-----------+
+            | dvec2     | 2         |
+            +-----------+-----------+
+            | dvec3     | 3         |
+            +-----------+-----------+
+            | dvec4     | 4         |
+            +-----------+-----------+
+            | mat2      | 4         |
+            +-----------+-----------+
+            | mat2x3    | 6         |
+            +-----------+-----------+
+            | mat2x4    | 8         |
+            +-----------+-----------+
+            | mat3x2    | 6         |
+            +-----------+-----------+
+            | mat3      | 9         |
+            +-----------+-----------+
+            | mat3x4    | 12        |
+            +-----------+-----------+
+            | mat4x2    | 8         |
+            +-----------+-----------+
+            | mat4x3    | 12        |
+            +-----------+-----------+
+            | mat4      | 16        |
+            +-----------+-----------+
+            | dmat2     | 4         |
+            +-----------+-----------+
+            | dmat2x3   | 6         |
+            +-----------+-----------+
+            | dmat2x4   | 8         |
+            +-----------+-----------+
+            | dmat3x2   | 6         |
+            +-----------+-----------+
+            | dmat3     | 9         |
+            +-----------+-----------+
+            | dmat3x4   | 12        |
+            +-----------+-----------+
+            | dmat4x2   | 8         |
+            +-----------+-----------+
+            | dmat4x3   | 12        |
+            +-----------+-----------+
+            | dmat4     | 16        |
+            +-----------+-----------+
         '''
 
         return self.mglo.dimension
@@ -227,7 +302,37 @@ class Attribute:
     @property
     def shape(self):
         '''
-            shape
+            The shape is a single character, representing the scalar type of the attriute.
+
+            +---------+--------------------------+
+            | shape   | GLSL types               |
+            +=========+==========================+
+            | ``'i'`` | int                      |
+            |         +--------------------------+
+            |         | ivec2 ivec3 ivec4        |
+            +---------+--------------------------+
+            | ``'I'`` | uint                     |
+            |         +--------------------------+
+            |         | uvec2 uvec3 uvec4        |
+            +---------+--------------------------+
+            | ``'f'`` | float                    |
+            |         +--------------------------+
+            |         | vec2 vec3 vec4           |
+            |         +--------------------------+
+            |         | mat2 mat3 mat4           |
+            |         +--------------------------+
+            |         | mat2x3 mat2x4 mat3x4     |
+            |         | mat4x2 mat4x2 mat4x3     |
+            +---------+--------------------------+
+            | ``'d'`` | double                   |
+            |         +--------------------------+
+            |         | dvec2 dvec3 dvec4        |
+            |         +--------------------------+
+            |         | dmat2 dmat3 dmat4        |
+            |         +--------------------------+
+            |         | dmat2x3 dmat2x4 dmat3x4  |
+            |         | dmat4x2 dmat4x2 dmat4x3  |
+            +---------+--------------------------+
         '''
 
         return self.mglo.shape
--- src/Attribute.cpp
+++ src/Attribute.cpp
@@ -44,156 +44,21 @@ PyObject * MGLAttribute_get_name(MGLAttribute * self, void * closure) {
 	return self->name;
 }
 
-char MGLAttribute_name_doc[] = R"(
-	name
-
-	The attribute name.
-	The name will be filtered to have no array syntax on it's end.
-	Attribute name without ``'[0]'`` ending if any.
-)";
-
 PyObject * MGLAttribute_get_location(MGLAttribute * self, void * closure) {
 	return PyLong_FromLong(self->location);
 }
-
-char MGLAttribute_location_doc[] = R"(
-	location
-
-	The attribute location.
-	The result of the glGetAttribLocation.
-)";
 
 PyObject * MGLAttribute_get_array_length(MGLAttribute * self, void * closure) {
 	return PyLong_FromLong(self->array_length);
 }
 
-char MGLAttribute_array_length_doc[] = R"(
-	array_length
-
-	If the attribute is an array the array_length is the length of the array otherwise it is ``1``.
-)";
-
 PyObject * MGLAttribute_get_dimension(MGLAttribute * self, void * closure) {
 	return PyLong_FromLong(self->dimension);
 }
 
-char MGLAttribute_dimension_doc[] = R"(
-	dimension
-
-	The attribute dimension.
-
-	+-----------+-----------+
-	| GLSL type | dimension |
-	+===========+===========+
-	| int       | 1         |
-	+-----------+-----------+
-	| ivec2     | 2         |
-	+-----------+-----------+
-	| ivec3     | 3         |
-	+-----------+-----------+
-	| ivec4     | 4         |
-	+-----------+-----------+
-	| uint      | 1         |
-	+-----------+-----------+
-	| uvec2     | 2         |
-	+-----------+-----------+
-	| uvec3     | 3         |
-	+-----------+-----------+
-	| uvec4     | 4         |
-	+-----------+-----------+
-	| float     | 1         |
-	+-----------+-----------+
-	| vec2      | 2         |
-	+-----------+-----------+
-	| vec3      | 3         |
-	+-----------+-----------+
-	| vec4      | 4         |
-	+-----------+-----------+
-	| double    | 1         |
-	+-----------+-----------+
-	| dvec2     | 2         |
-	+-----------+-----------+
-	| dvec3     | 3         |
-	+-----------+-----------+
-	| dvec4     | 4         |
-	+-----------+-----------+
-	| mat2      | 4         |
-	+-----------+-----------+
-	| mat2x3    | 6         |
-	+-----------+-----------+
-	| mat2x4    | 8         |
-	+-----------+-----------+
-	| mat3x2    | 6         |
-	+-----------+-----------+
-	| mat3      | 9         |
-	+-----------+-----------+
-	| mat3x4    | 12        |
-	+-----------+-----------+
-	| mat4x2    | 8         |
-	+-----------+-----------+
-	| mat4x3    | 12        |
-	+-----------+-----------+
-	| mat4      | 16        |
-	+-----------+-----------+
-	| dmat2     | 4         |
-	+-----------+-----------+
-	| dmat2x3   | 6         |
-	+-----------+-----------+
-	| dmat2x4   | 8         |
-	+-----------+-----------+
-	| dmat3x2   | 6         |
-	+-----------+-----------+
-	| dmat3     | 9         |
-	+-----------+-----------+
-	| dmat3x4   | 12        |
-	+-----------+-----------+
-	| dmat4x2   | 8         |
-	+-----------+-----------+
-	| dmat4x3   | 12        |
-	+-----------+-----------+
-	| dmat4     | 16        |
-	+-----------+-----------+
-)";
-
 PyObject * MGLAttribute_get_shape(MGLAttribute * self, void * closure) {
 	return PyUnicode_FromFormat("%c", self->shape);
 }
-
-char MGLAttribute_shape_doc[] = R"(
-	shape
-
-	The shape is a single character that represents the scalar type of the attriute.
-
-	+---------+--------------------------+
-	| shape   | GLSL types               |
-	+=========+==========================+
-	| ``'i'`` | int                      |
-	|         +--------------------------+
-	|         | ivec2 ivec3 ivec4        |
-	+---------+--------------------------+
-	| ``'I'`` | uint                     |
-	|         +--------------------------+
-	|         | uvec2 uvec3 uvec4        |
-	+---------+--------------------------+
-	| ``'f'`` | float                    |
-	|         +--------------------------+
-	|         | vec2 vec3 vec4           |
-	|         +--------------------------+
-	|         | mat2 mat3 mat4           |
-	|         +--------------------------+
-	|         | mat2x3 mat2x4 mat3x4     |
-	|         | mat4x2 mat4x2 mat4x3     |
-	+---------+--------------------------+
-	| ``'d'`` | double                   |
-	|         +--------------------------+
-	|         | dvec2 dvec3 dvec4        |
-	|         +--------------------------+
-	|         | dmat2 dmat3 dmat4        |
-	|         +--------------------------+
-	|         | dmat2x3 dmat2x4 dmat3x4  |
-	|         | dmat4x2 dmat4x2 dmat4x3  |
-	+---------+--------------------------+
-)";
 
 PyGetSetDef MGLAttribute_tp_getseters[] = {
 	{(char *)"name", (getter)MGLAttribute_get_name, 0, 0, 0},
