93db7f4dadf61203d065ef029fb10da24c191f52
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-25 21:43:07 +0200

    UniformBlock test

--- tests/test_uniform_block.py
+++ tests/test_uniform_block.py
@@ -13,17 +13,23 @@ class TestCase(unittest.TestCase):
         self.vert = self.ctx.vertex_shader('''
             #version 330
 
-            in mat2 in_m;
             in vec2 in_v;
-
             out vec2 out_v;
 
-            uniform Block {
+            uniform Block1 {
                 float x;
             };
 
+            uniform Block2 {
+                float y;
+            };
+
+            uniform Block3 {
+                float z;
+            };
+
             void main() {
-                out_v = in_m * in_v * x;
+                out_v = in_v * z + vec2(x, y);
             }
         ''')
 
@@ -34,27 +40,46 @@ class TestCase(unittest.TestCase):
         self.ctx.release()
 
     def test_1(self):
-        buf_m = self.ctx.buffer(struct.pack('4f', 1, 1, 1, 2))
-        buf_v = self.ctx.buffer(struct.pack('2f', 4, 7))
-        buf_u = self.ctx.buffer(struct.pack('f', 123))
+        buf_v = self.ctx.buffer(struct.pack('2f', 100.0, 1000.0))
+        buf_u1 = self.ctx.buffer(struct.pack('f', 9.5))
+        buf_u2 = self.ctx.buffer(struct.pack('f', 4.0))
+        buf_u3 = self.ctx.buffer(struct.pack('f', 3.0))
         buf_r = self.ctx.buffer(reserve=buf_v.size)
-        res = self.ctx.buffer(reserve=buf_v.size)
 
         vao = self.ctx.vertex_array(self.prog, [
-            (buf_m, '4f', ['in_m']),
             (buf_v, '2f', ['in_v']),
         ])
 
-        self.assertFalse('Block' in self.prog.uniforms)
-        self.assertTrue('Block' in self.prog.uniform_blocks)
+        self.assertFalse('Block1' in self.prog.uniforms)
+        self.assertFalse('Block2' in self.prog.uniforms)
+        self.assertFalse('Block3' in self.prog.uniforms)
 
-        buf_u.bind_to_uniform_block(self.prog.uniform_blocks['Block'])
+        self.assertFalse('x' in self.prog.uniforms)
+        self.assertFalse('y' in self.prog.uniforms)
+        self.assertFalse('z' in self.prog.uniforms)
 
-        vao.transform(buf_r, ModernGL.POINTS)
+        self.assertTrue('Block1' in self.prog.uniform_blocks)
+        self.assertTrue('Block2' in self.prog.uniform_blocks)
+        self.assertTrue('Block3' in self.prog.uniform_blocks)
+
+        self.prog.uniform_blocks['Block1'].binding = 2
+        self.prog.uniform_blocks['Block2'].binding = 4
+        self.prog.uniform_blocks['Block3'].binding = 1
+
+        buf_u1.bind_to_uniform_block(2)
+        buf_u2.bind_to_uniform_block(4)
+
+        vao.transform(buf_r)
         a, b = struct.unpack('2f', buf_r.read())
+        self.assertAlmostEqual(a, 9.5)
+        self.assertAlmostEqual(b, 4.0)
 
-        self.assertAlmostEqual(a, 1353.0)
-        self.assertAlmostEqual(b, 2214.0)
+        buf_u3.bind_to_uniform_block(1)
+
+        vao.transform(buf_r)
+        a, b = struct.unpack('2f', buf_r.read())
+        self.assertAlmostEqual(a, 309.5)
+        self.assertAlmostEqual(b, 3004.0)
 
 
 if __name__ == '__main__':
