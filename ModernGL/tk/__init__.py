033ab79a210e9befddb51627600e01ace2400d89
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-28 22:35:11 +0200

    tkinter windows connector

---
+++ ModernGL/tk/__init__.py
@@ -0,0 +1,81 @@
+import struct
+
+import ctypes
+import tkinter
+
+
+class ModernGLCanvasError(Exception):
+    '''
+        Custom error type for ModernGLCanvas
+    '''
+
+
+class ModernGLCanvas(tkinter.Frame):
+    '''
+        The tkinter ModernGL connector
+    '''
+
+    def __init__(self, root, width, height):
+        tkinter.Frame.__init__(self, root, width=width, height=height, bg="", colormap="new")
+
+        self._width = width
+        self._height = height
+
+        # typedef struct tagBITMAPINFOHEADER {
+        #     DWORD biSize;
+        #     LONG biWidth;
+        #     LONG biHeight;
+        #     WORD biPlanes;
+        #     WORD biBitCount;
+        #     DWORD biCompression;
+        #     DWORD biSizeImage;
+        #     LONG biXPelsPerMeter;
+        #     LONG biYPelsPerMeter;
+        #     DWORD biClrUsed;
+        #     DWORD biClrImportant;
+        # } BITMAPINFOHEADER;
+
+        hbmpi = struct.pack('IIIHHIIIIII', 40, width, height, 1, 24, 0, 0, 0, 0, 0, 0)
+
+        self._hwnd = self.winfo_id()
+
+        if not self._hwnd:
+            raise ModernGLCanvasError('cannot get the hwnd')
+
+        self._hdc = ctypes.windll.user32.GetDC(self._hwnd)
+
+        if not self._hdc:
+            raise ModernGLCanvasError('cannot get the hdc')
+
+        self._hmemdc = ctypes.windll.gdi32.CreateCompatibleDC(self._hdc)
+
+        if not self._hmemdc:
+            raise ModernGLCanvasError('cannot create device content')
+
+        bits = ctypes.c_void_p()
+        self._hbmp = ctypes.windll.gdi32.CreateDIBSection(self._hmemdc, hbmpi, 0, ctypes.byref(bits), 0, 0)
+
+        if not self._hbmp:
+            raise ModernGLCanvasError('cannot create bitmap section')
+
+        if not bits.value:
+            raise ModernGLCanvasError('cannot access the pixel data')
+
+        array_type = (ctypes.c_char * width * height * 3)
+        self._pixels = array_type.from_address(bits.value)
+
+        sel = ctypes.windll.gdi32.SelectObject(self._hmemdc, self._hbmp)
+
+        if not sel:
+            raise ModernGLCanvasError('cannot select bitmap')
+
+    @property
+    def pixels(self):
+        '''
+            The pixels.
+        '''
+
+        return self._pixels
+
+    def update(self) -> None:
+        ctypes.windll.gdi32.BitBlt(self._hdc, 0, 0, self._width, self._height, self._hmemdc, 0, 0, 0x00CC0020)
---
+++ examples/tkinter/01_hello_world.py
@@ -0,0 +1,105 @@
+import ModernGL
+import ModernGL.tk
+import tkinter, struct
+
+ctx = ModernGL.create_standalone_context()
+
+
+prog = ctx.program([
+    ctx.vertex_shader('''
+        #version 330
+
+        in vec2 vert;
+
+        in vec3 vert_color;
+        out vec3 frag_color;
+
+        uniform vec2 scale;
+        uniform float rotation;
+
+        void main() {
+            frag_color = vert_color;
+            mat2 rot = mat2(
+                cos(rotation), sin(rotation),
+                -sin(rotation), cos(rotation)
+            );
+            gl_Position = vec4((rot * vert) * scale, 0.0, 1.0);
+        }
+    '''),
+    ctx.fragment_shader('''
+        #version 330
+
+        in vec3 frag_color;
+        out vec4 color;
+
+        void main() {
+            color = vec4(frag_color, 1.0);
+        }
+    '''),
+])
+
+# Uniforms
+
+scale = prog.uniforms['scale']
+rotation = prog.uniforms['rotation']
+
+width, height = 512, 512
+scale.value = (height / width * 0.75, 0.75)
+
+# Buffer
+
+vbo = ctx.buffer(struct.pack(
+    '15f',
+
+    1.0, 0.0,
+    1.0, 0.0, 0.0,
+
+    -0.5, 0.86,
+    0.0, 1.0, 0.0,
+
+    -0.5, -0.86,
+    0.0, 0.0, 1.0,
+))
+
+# Put everything together
+
+vao = ctx.simple_vertex_array(prog, vbo, ['vert', 'vert_color'])
+
+# Main loop
+
+
+size = (512, 512)
+color_rbo = ctx.renderbuffer(size)
+depth_rbo = ctx.depth_renderbuffer(size)
+fbo = ctx.framebuffer(color_rbo, depth_rbo)
+
+root = tkinter.Tk()
+
+# frame = tkinter.Frame(root, width=512, height=512, bg="", colormap="new")
+frame = ModernGL.tk.ModernGLCanvas(root, 512, 512)
+frame.pack()
+
+# connector = ModernGL.tk.connect(frame)
+R = tkinter.Scale(root, from_=0, to=255, orient=tkinter.HORIZONTAL, length=300)
+R.pack()
+
+k = 1
+
+def task():
+    global k
+    k += 0.02
+    fbo.use()
+    ctx.viewport = (0, 0, 512, 512)
+    ctx.clear(0.9, 0.9, 0.9)
+    rotation.value = k
+    vao.render()
+
+    fbo.read_into(frame.pixels, components=3)
+    frame.update()
+
+    root.after(1, task)
+
+root.after(1, task)
+
+tkinter.mainloop()
+
