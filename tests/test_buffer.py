78af4d2ac5b8e4e013bcc9d2243af194807db6c9
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-06-07 15:37:09 +0200

    remove duplicate tearDown

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -16,9 +16,6 @@ class TestBuffer(unittest.TestCase):
     def tearDown(self):
         self.assertEqual(self.ctx.error, 'GL_NO_ERROR')
 
-    def tearDown(self):
-        self.assertEqual(self.ctx.error, 'GL_NO_ERROR')
-
     def test_buffer_clear_1(self):
         buf = self.ctx.buffer(data=b'\xAA\x55' * 10)
         buf.clear(chunk=b'AB')
