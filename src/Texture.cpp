73ae76f66cfe33d3cd70bb7d134cb857be090cfc
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-06-19 04:56:59 +0200

    fix-alignment

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -74,6 +74,9 @@ PyObject * MGLTexture_read(MGLTexture * self, PyObject * args) {
 	gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
 	gl.BindTexture(texture_target, self->texture_obj);
 
+	// TODO: GL_PACK_ALIGNMENT or GL_UNPACK_ALIGNMENT not sure
+
+	gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
 	gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 	gl.GetTexImage(texture_target, 0, format, pixel_type, data);
 
@@ -126,6 +129,10 @@ PyObject * MGLTexture_read_into(MGLTexture * self, PyObject * args) {
 		gl.BindBuffer(GL_PIXEL_PACK_BUFFER, buffer->buffer_obj);
 		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
 		gl.BindTexture(texture_target, self->texture_obj);
+
+		// TODO: GL_PACK_ALIGNMENT or GL_UNPACK_ALIGNMENT not sure
+
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
 		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 		gl.GetTexImage(texture_target, 0, format, pixel_type, (void *)write_offset);
 		gl.BindBuffer(GL_PIXEL_PACK_BUFFER, 0);
@@ -151,6 +158,10 @@ PyObject * MGLTexture_read_into(MGLTexture * self, PyObject * args) {
 		const GLMethods & gl = self->context->gl;
 		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
 		gl.BindTexture(texture_target, self->texture_obj);
+
+		// TODO: GL_PACK_ALIGNMENT or GL_UNPACK_ALIGNMENT not sure
+
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
 		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 		gl.GetTexImage(texture_target, 0, format, pixel_type, ptr);
 
@@ -246,7 +257,11 @@ PyObject * MGLTexture_write(MGLTexture * self, PyObject * args) {
 		gl.BindBuffer(GL_PIXEL_UNPACK_BUFFER, buffer->buffer_obj);
 		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
 		gl.BindTexture(texture_target, self->texture_obj);
+
+		// TODO: GL_PACK_ALIGNMENT or GL_UNPACK_ALIGNMENT not sure
+
 		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 		gl.TexSubImage2D(texture_target, 0, x, y, width, height, format, pixel_type, 0);
 		gl.BindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
@@ -271,7 +286,10 @@ PyObject * MGLTexture_write(MGLTexture * self, PyObject * args) {
 		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
 		gl.BindTexture(texture_target, self->texture_obj);
 
+		// TODO: GL_PACK_ALIGNMENT or GL_UNPACK_ALIGNMENT not sure
+
 		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 		gl.TexSubImage2D(texture_target, 0, x, y, width, height, format, pixel_type, buffer_view.buf);
 
 		PyBuffer_Release(&buffer_view);
