700d0147b94565a669cb8a001030d8f104d78b2b
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-04 21:13:50 +0300

    fix snippet generator

--- extras/vscode/create_snippet.py
+++ extras/vscode/create_snippet.py
@@ -2,5 +2,5 @@ import json
 import sys
 
 text = open(sys.argv[1], 'r').read()
-lines = text.replace(' ' * 4, '\t').splitlines()
+lines = text.replace(' ' * 4, '\t').splitlines() + ['']
 print(json.dumps(lines, indent=4))
