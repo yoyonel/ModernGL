a1aeef50047887c97cee7835fb0599364828f4ea
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-04 21:27:32 +0300

    better snippet generator

--- extras/vscode/create_snippet.py
+++ extras/vscode/create_snippet.py
@@ -3,4 +3,12 @@ import sys
 
 text = open(sys.argv[1], 'r').read()
 lines = text.replace(' ' * 4, '\t').splitlines() + ['']
-print(json.dumps(lines, indent=4))
+
+snippet = {
+	"ModernGL Snippet": {
+		"prefix": "mgl_new_example_",
+		"body": lines,
+    }
+}
+
+print(json.dumps(snippet, indent=4))
