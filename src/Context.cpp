ea3e227b98d59813fac19d73ebf8f53ab54bf271
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-08 14:20:39 +0300

    register new method

--- src/Context.cpp
+++ src/Context.cpp
@@ -1690,6 +1690,7 @@ PyMethodDef MGLContext_tp_methods[] = {
 	{"buffer", (PyCFunction)MGLContext_buffer, METH_VARARGS, 0},
 	{"texture", (PyCFunction)MGLContext_texture, METH_VARARGS, 0},
 	{"texture3d", (PyCFunction)MGLContext_texture3d, METH_VARARGS, 0},
+	{"texture_cube", (PyCFunction)MGLContext_texture_cube, METH_VARARGS, 0},
 	{"depth_texture", (PyCFunction)MGLContext_depth_texture, METH_VARARGS, 0},
 	{"vertex_array", (PyCFunction)MGLContext_vertex_array, METH_VARARGS, 0},
 	{"program", (PyCFunction)MGLContext_program, METH_VARARGS, 0},
