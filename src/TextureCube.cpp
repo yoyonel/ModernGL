a4e4d2cd8784842ec516020d380fc64df51c7413
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-26 20:35:17 +0300

    read and read_into in C

--- src/TextureCube.cpp
+++ src/TextureCube.cpp
@@ -33,6 +33,141 @@ int MGLTextureCube_tp_init(MGLTextureCube * self, PyObject * args, PyObject * kw
 	return -1;
 }
 
+PyObject * MGLTextureCube_read(MGLTextureCube * self, PyObject * args) {
+	int alignment;
+
+	int args_ok = PyArg_ParseTuple(
+		args,
+		"I",
+		&alignment
+	);
+
+	if (!args_ok) {
+		return 0;
+	}
+
+	if (alignment != 1 && alignment != 2 && alignment != 4 && alignment != 8) {
+		MGLError_Set("the alignment must be 1, 2, 4 or 8");
+		return 0;
+	}
+
+	int expected_size = self->width * self->components * (self->floats ? 4 : 1);
+	expected_size = (expected_size + alignment - 1) / alignment * alignment;
+	expected_size = expected_size * self->height * 6;
+
+	PyObject * result = PyBytes_FromStringAndSize(0, expected_size);
+	char * data = PyBytes_AS_STRING(result);
+
+	const int formats[] = {0, GL_RED, GL_RG, GL_RGB, GL_RGBA};
+
+	int pixel_type = self->floats ? GL_FLOAT : GL_UNSIGNED_BYTE;
+	int format = formats[self->components];
+
+	const GLMethods & gl = self->context->gl;
+
+	gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+	gl.BindTexture(GL_TEXTURE_CUBE_MAP, self->texture_obj);
+
+	gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+	gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, format, pixel_type, data + expected_size * 0 / 6);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, format, pixel_type, data + expected_size * 1 / 6);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, format, pixel_type, data + expected_size * 2 / 6);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, format, pixel_type, data + expected_size * 3 / 6);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, format, pixel_type, data + expected_size * 4 / 6);
+	gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, format, pixel_type, data + expected_size * 5 / 6);
+
+	return result;
+}
+
+
+PyObject * MGLTextureCube_read_into(MGLTextureCube * self, PyObject * args) {
+	PyObject * data;
+	int alignment;
+	Py_ssize_t write_offset;
+
+	int args_ok = PyArg_ParseTuple(
+		args,
+		"OIn",
+		&data,
+		&alignment,
+		&write_offset
+	);
+
+	if (!args_ok) {
+		return 0;
+	}
+
+	if (alignment != 1 && alignment != 2 && alignment != 4 && alignment != 8) {
+		MGLError_Set("the alignment must be 1, 2, 4 or 8");
+		return 0;
+	}
+
+	int expected_size = self->width * self->components * (self->floats ? 4 : 1);
+	expected_size = (expected_size + alignment - 1) / alignment * alignment;
+	expected_size = expected_size * self->height * 6;
+
+	const int formats[] = {0, GL_RED, GL_RG, GL_RGB, GL_RGBA};
+
+	int pixel_type = self->floats ? GL_FLOAT : GL_UNSIGNED_BYTE;
+	int format = formats[self->components];
+
+	if (Py_TYPE(data) == &MGLBuffer_Type) {
+
+		MGLBuffer * buffer = (MGLBuffer *)data;
+
+		const GLMethods & gl = self->context->gl;
+
+		gl.BindBuffer(GL_PIXEL_PACK_BUFFER, buffer->buffer_obj);
+		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+		gl.BindTexture(GL_TEXTURE_CUBE_MAP, self->texture_obj);
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, format, pixel_type, (char *)write_offset + expected_size * 0 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, format, pixel_type, (char *)write_offset + expected_size * 1 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, format, pixel_type, (char *)write_offset + expected_size * 2 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, format, pixel_type, (char *)write_offset + expected_size * 3 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, format, pixel_type, (char *)write_offset + expected_size * 4 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, format, pixel_type, (char *)write_offset + expected_size * 5 / 6);
+		gl.BindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+
+	} else {
+
+		Py_buffer buffer_view;
+
+		int get_buffer = PyObject_GetBuffer(data, &buffer_view, PyBUF_WRITABLE);
+		if (get_buffer < 0) {
+			MGLError_Set("the buffer (%s) does not support buffer interface", Py_TYPE(data)->tp_name);
+			return 0;
+		}
+
+		if (buffer_view.len < write_offset + expected_size) {
+			MGLError_Set("the buffer is too small");
+			PyBuffer_Release(&buffer_view);
+			return 0;
+		}
+
+		char * ptr = (char *)buffer_view.buf + write_offset;
+
+		const GLMethods & gl = self->context->gl;
+		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+		gl.BindTexture(GL_TEXTURE_CUBE_MAP, self->texture_obj);
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, format, pixel_type, ptr + expected_size * 0 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, format, pixel_type, ptr + expected_size * 1 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, format, pixel_type, ptr + expected_size * 2 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, format, pixel_type, ptr + expected_size * 3 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, format, pixel_type, ptr + expected_size * 4 / 6);
+		gl.GetTexImage(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, format, pixel_type, ptr + expected_size * 5 / 6);
+
+		PyBuffer_Release(&buffer_view);
+
+	}
+
+	Py_RETURN_NONE;
+}
+
 PyObject * MGLTextureCube_use(MGLTextureCube * self, PyObject * args) {
 	int index;
 
