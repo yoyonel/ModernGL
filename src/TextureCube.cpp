17b0a4b5b03f00156444b43e61609468a9d221b2
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-26 20:54:59 +0300

    write texture cube in C

--- src/TextureCube.cpp
+++ src/TextureCube.cpp
@@ -182,6 +182,137 @@ PyObject * MGLTextureCube_read_into(MGLTextureCube * self, PyObject * args) {
 	Py_RETURN_NONE;
 }
 
+PyObject * MGLTextureCube_write(MGLTextureCube * self, PyObject * args) {
+	int face;
+	PyObject * data;
+	PyObject * viewport;
+	int alignment;
+
+	int args_ok = PyArg_ParseTuple(
+		args,
+		"iOOI",
+		&face,
+		&data,
+		&viewport,
+		&alignment
+	);
+
+	if (!args_ok) {
+		return 0;
+	}
+
+	if (face < 0 || face > 5) {
+		MGLError_Set("the face must be 0, 1, 2, 3, 4 or 5");
+		return 0;
+	}
+
+	if (alignment != 1 && alignment != 2 && alignment != 4 && alignment != 8) {
+		MGLError_Set("the alignment must be 1, 2, 4 or 8");
+		return 0;
+	}
+
+	int x = 0;
+	int y = 0;
+	int width = self->width;
+	int height = self->height;
+
+	Py_buffer buffer_view;
+
+	if (viewport != Py_None) {
+		if (Py_TYPE(viewport) != &PyTuple_Type) {
+			MGLError_Set("the viewport must be a tuple not %s", Py_TYPE(viewport)->tp_name);
+			return 0;
+		}
+
+		if (PyTuple_GET_SIZE(viewport) == 4) {
+
+			x = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 0));
+			y = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 1));
+			width = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 2));
+			height = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 3));
+
+		} else if (PyTuple_GET_SIZE(viewport) == 2) {
+
+			width = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 0));
+			height = PyLong_AsLong(PyTuple_GET_ITEM(viewport, 1));
+
+		} else {
+
+			MGLError_Set("the viewport size %d is invalid", PyTuple_GET_SIZE(viewport));
+			return 0;
+
+		}
+
+		if (PyErr_Occurred()) {
+			MGLError_Set("wrong values in the viewport");
+			return 0;
+		}
+
+	}
+
+	int expected_size = width * self->components * (self->floats ? 4 : 1);
+	expected_size = (expected_size + alignment - 1) / alignment * alignment;
+	expected_size = expected_size * height;
+
+	const int formats[] = {0, GL_RED, GL_RG, GL_RGB, GL_RGBA};
+	const int faces[] = {
+		GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+	};
+
+	int pixel_type = self->floats ? GL_FLOAT : GL_UNSIGNED_BYTE;
+	int format = formats[self->components];
+
+	if (Py_TYPE(data) == &MGLBuffer_Type) {
+
+		MGLBuffer * buffer = (MGLBuffer *)data;
+
+		const GLMethods & gl = self->context->gl;
+
+		gl.BindBuffer(GL_PIXEL_UNPACK_BUFFER, buffer->buffer_obj);
+		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+		gl.BindTexture(GL_TEXTURE_CUBE_MAP, self->texture_obj);
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+		gl.TexSubImage2D(faces[face], 0, x, y, width, height, format, pixel_type, 0);
+		gl.BindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+	} else {
+
+		int get_buffer = PyObject_GetBuffer(data, &buffer_view, PyBUF_SIMPLE);
+		if (get_buffer < 0) {
+			MGLError_Set("data (%s) does not support buffer interface", Py_TYPE(data)->tp_name);
+			return 0;
+		}
+
+		if (buffer_view.len != expected_size) {
+			MGLError_Set("data size mismatch %d != %d", buffer_view.len, expected_size);
+			if (data != Py_None) {
+				PyBuffer_Release(&buffer_view);
+			}
+			return 0;
+		}
+
+		const GLMethods & gl = self->context->gl;
+
+		gl.ActiveTexture(GL_TEXTURE0 + self->context->default_texture_unit);
+		gl.BindTexture(GL_TEXTURE_CUBE_MAP, self->texture_obj);
+
+		gl.PixelStorei(GL_PACK_ALIGNMENT, alignment);
+		gl.PixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+		gl.TexSubImage2D(faces[face], 0, x, y, width, height, format, pixel_type, buffer_view.buf);
+
+		PyBuffer_Release(&buffer_view);
+
+	}
+
+	Py_RETURN_NONE;
+}
+
 PyObject * MGLTextureCube_use(MGLTextureCube * self, PyObject * args) {
 	int index;
 
@@ -210,6 +341,7 @@ PyObject * MGLTextureCube_release(MGLTextureCube * self) {
 PyMethodDef MGLTextureCube_tp_methods[] = {
 	{"read", (PyCFunction)MGLTextureCube_read, METH_VARARGS, 0},
 	{"read_into", (PyCFunction)MGLTextureCube_read_into, METH_VARARGS, 0},
+	{"write", (PyCFunction)MGLTextureCube_write, METH_VARARGS, 0},
 	{"use", (PyCFunction)MGLTextureCube_use, METH_VARARGS, 0},
 	{"release", (PyCFunction)MGLTextureCube_release, METH_NOARGS, 0},
 	{0},
