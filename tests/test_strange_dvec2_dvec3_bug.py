4b468be3de899c28a95879a1e0993f04a79133d8
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-08-18 21:15:02 +0300

    strange bug on integrated gpu

---
+++ tests/test_strange_dvec2_dvec3_bug.py
@@ -0,0 +1,73 @@
+import struct
+import unittest
+
+import ModernGL
+
+
+class TestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ctx = ModernGL.create_standalone_context()
+
+    def tearDown(self):
+        self.assertEqual(self.ctx.error, 'GL_NO_ERROR')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.ctx.release()
+
+    def test_dvec2(self):
+        self.prog = self.ctx.program([
+            self.ctx.vertex_shader('''
+                #version 410
+
+                in dvec2 in_a[2];
+                out dvec2 out_a[2];
+
+                void main() {
+                    out_a[0] = in_a[0];
+                    out_a[1] = in_a[1];
+                }
+            '''),
+        ], ['out_a'])
+
+        self.vbo1 = self.ctx.buffer(struct.pack('2d2d', 1.0, 1.0, 1.0, 1.0))
+        self.vbo2 = self.ctx.buffer(struct.pack('2d2d', 0.0, 0.0, 0.0, 0.0))
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo1, ['in_a'])
+        self.vao.transform(self.vbo2, ModernGL.POINTS, 1)
+        x1, y1, x2, y2 = struct.unpack('2d2d', self.vbo2.read())
+        self.assertAlmostEquals(x1, 1.0)
+        self.assertAlmostEquals(y1, 1.0)
+        self.assertAlmostEquals(x2, 1.0)
+        self.assertAlmostEquals(y2, 1.0)
+
+    def test_dvec3(self):
+        self.prog = self.ctx.program([
+            self.ctx.vertex_shader('''
+                #version 410
+
+                in dvec3 in_a[2];
+                out dvec3 out_a[2];
+
+                void main() {
+                    out_a[0] = in_a[0];
+                    out_a[1] = in_a[1];
+                }
+            '''),
+        ], ['out_a'])
+
+        self.vbo1 = self.ctx.buffer(struct.pack('3d3d', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0))
+        self.vbo2 = self.ctx.buffer(struct.pack('3d3d', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo1, ['in_a'])
+        self.vao.transform(self.vbo2, ModernGL.POINTS, 1)
+        x1, y1, z1, x2, y2, z2 = struct.unpack('3d3d', self.vbo2.read())
+        self.assertAlmostEquals(x1, 1.0)
+        self.assertAlmostEquals(y1, 1.0)
+        self.assertAlmostEquals(z1, 1.0)
+        self.assertAlmostEquals(x2, 1.0)
+        self.assertAlmostEquals(y2, 1.0)
+        self.assertAlmostEquals(z2, 1.0)
+
+if __name__ == '__main__':
+    unittest.main()
