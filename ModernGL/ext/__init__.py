267067d03776576106f63f5b13e17533b7a0d0e9
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-08-18 18:51:24 +0300

    extension finder

--- ModernGL/ext/__init__.py
+++ ModernGL/ext/__init__.py
@@ -1,3 +1,33 @@
 '''
     ModernGL extensions
 '''
+
+import os
+import sys
+
+
+class ExtensionFinder():
+    def find_spec(self, fullname, path, target=None):
+        prefix = 'ModernGL.ext.'
+
+        if fullname.startswith(prefix):
+            name = fullname[len(prefix):]
+            if '.' in name:
+                return
+
+            here = os.path.dirname(__file__)
+            path = os.path.join(here, name)
+
+            if not os.path.isdir(path):
+                local = os.path.isfile(os.path.join(here, 'README.md'))
+
+                if local:
+                    error = 'missing extension\n\nDo not run ModernGL from the git repository!'
+
+                else:
+                    error = 'missing extension\n\nHint: python3 -m pip install %s' % fullname
+
+                raise ModuleNotFoundError(error)
+
+
+sys.meta_path.append(ExtensionFinder())
