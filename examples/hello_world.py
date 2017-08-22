08c7a65bc74d8f23b029afa83bf17be9468f48f5
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-08-22 17:37:16 +0300

    hello world

---
+++ examples/hello_world.py
@@ -0,0 +1,47 @@
+import ModernGL
+from ModernGL.ext.examples import run_example
+import numpy as np
+
+
+class Example:
+    def __init__(self, wnd):
+        self.wnd = wnd
+        self.ctx = ModernGL.create_context()
+
+        self.prog = self.ctx.program([
+            self.ctx.vertex_shader('''
+                #version 330
+
+                in vec2 in_vert;
+
+                void main() {
+                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                }
+            '''),
+            self.ctx.fragment_shader('''
+                #version 330
+
+                out vec4 f_color;
+
+                void main() {
+                    f_color = vec4(0.3, 0.5, 1.0, 1.0);
+                }
+            '''),
+        ])
+
+        vertices = np.array([
+            0.0, 0.8,
+            -0.6, -0.8,
+            0.6, -0.8,
+        ])
+
+        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, ['in_vert'])
+
+    def render(self):
+        self.ctx.viewport = self.wnd.viewport
+        self.ctx.clear(1.0, 1.0, 1.0)
+        self.vao.render()
+
+
+run_example(Example)
