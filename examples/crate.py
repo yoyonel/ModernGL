aafcd97b530279a9a05743c411ea77d0c4a675d4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-08-23 20:10:34 +0300

    crate example

---
+++ examples/crate.py
@@ -0,0 +1,92 @@
+import os
+
+import ModernGL
+import numpy as np
+from ModernGL.ext.obj import Obj
+from ModernGL.ext.examples import run_example
+from PIL import Image
+from pyrr import Matrix44
+
+
+def local(*path):
+    return os.path.join(os.path.dirname(__file__), *path)
+
+
+class Example:
+    def __init__(self, wnd):
+        self.wnd = wnd
+        self.ctx = ModernGL.create_context()
+
+        self.prog = self.ctx.program([
+            self.ctx.vertex_shader('''
+                #version 330
+
+                uniform mat4 Mvp;
+
+                in vec3 in_vert;
+                in vec3 in_norm;
+                in vec2 in_text;
+
+                out vec3 v_vert;
+                out vec3 v_norm;
+                out vec2 v_text;
+
+                void main() {
+                    gl_Position = Mvp * vec4(in_vert, 1.0);
+                    v_vert = in_vert;
+                    v_norm = in_norm;
+                    v_text = in_text;
+                }
+            '''),
+            self.ctx.fragment_shader('''
+                #version 330
+
+                uniform vec3 Light;
+                uniform sampler2D Texture;
+
+                in vec3 v_vert;
+                in vec3 v_norm;
+                in vec2 v_text;
+
+                out vec4 f_color;
+
+                void main() {
+                    float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.8 + 0.2;
+                    f_color = vec4(texture(Texture, v_text).rgb * lum, 1.0);
+                }
+            '''),
+        ])
+
+        self.mvp = self.prog.uniforms['Mvp']
+        self.light = self.prog.uniforms['Light']
+
+        obj = Obj.open(local('data', 'crate.obj'))
+        img = Image.open(local('data', 'crate.png')).transpose(Image.FLIP_TOP_BOTTOM).convert('RGB')
+        self.texture = self.ctx.texture(img.size, 3, img.tobytes())
+        self.texture.use()
+
+        self.vbo = self.ctx.buffer(obj.pack('vx vy vz nx ny nz tx ty'))
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, ['in_vert', 'in_norm', 'in_text'])
+
+    def render(self):
+        angle = self.wnd.time
+        width, height = self.wnd.size
+        self.ctx.viewport = self.wnd.viewport
+        self.ctx.clear(1.0, 1.0, 1.0)
+        self.ctx.enable(ModernGL.DEPTH_TEST)
+
+        camera_pos = (np.cos(angle) * 5.0, np.sin(angle) * 5.0, 2.0)
+
+        proj = Matrix44.perspective_projection(45.0, width / height, 0.1, 1000.0)
+        lookat = Matrix44.look_at(
+            camera_pos,
+            (0.0, 0.0, 0.5),
+            (0.0, 0.0, 1.0),
+        )
+
+        self.mvp.write((proj * lookat).astype('f4').tobytes())
+        self.light.value = camera_pos
+        self.vao.render()
+
+
+run_example(Example)
