c5c49c21c4abdaa8981859e8c5fc3f15764b38cc
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-10 14:18:02 +0300

    is local?

---
+++ tests/test_local.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+
+import ModernGL
+
+
+class TestCase(unittest.TestCase):
+
+    def test_local(self):
+        path1 = os.path.normpath(os.path.dirname(os.path.dirname(ModernGL.__file__)))
+        path2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.assertEqual(path1, path2, msg='Using installed ModernGL')
+
+
+if __name__ == '__main__':
+    unittest.main()
