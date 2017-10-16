8eb046445b8e024b8e4a5fd30959240c414e084e
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-17 00:09:06 +0300

    missing normpath

--- tests/test_local.py
+++ tests/test_local.py
@@ -8,7 +8,7 @@ class TestCase(unittest.TestCase):
 
     def test_local(self):
         path1 = os.path.normpath(os.path.dirname(os.path.dirname(ModernGL.__file__)))
-        path2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path2 = os.path.normpath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         self.assertEqual(path1, path2, msg='Using installed ModernGL')
 
 
