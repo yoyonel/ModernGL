e72800ffffb3869023d285bb383dda50ffb4d23b
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-11-13 19:50:31 +0200

    resize context before tests (debug)

--- tests/common.py
+++ tests/common.py
@@ -9,6 +9,7 @@ def get_context() -> ModernGL.Context:
     ctx = _static.get('context')
 
     if ctx is None:
+        ModernGL.mgl.set_default_context_size(100, 100)
         ctx = ModernGL.create_standalone_context()
         _static['context'] = ctx
 
