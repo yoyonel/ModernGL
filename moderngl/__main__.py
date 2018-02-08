d13c7d232ca4c18fb173d0522bfaa1572fc7fd2e
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-02-08 19:13:34 +0200

    fix main program

--- moderngl/__main__.py
+++ moderngl/__main__.py
@@ -7,7 +7,7 @@ import json
 import os
 import sys
 
-import ModernGL
+import moderngl
 
 
 def main(argv=None):
@@ -15,12 +15,12 @@ def main(argv=None):
         main
     '''
 
-    parser = argparse.ArgumentParser(prog='ModernGL')
-    parser.add_argument('-v', '--version', action='version', version='ModernGL %s' % ModernGL.__version__)
+    parser = argparse.ArgumentParser(prog='moderngl')
+    parser.add_argument('-v', '--version', action='version', version='moderngl %s' % moderngl.__version__)
     parser.add_argument('-i', '--info', action='store_true', default=False)
     args = parser.parse_args(argv)
 
-    ctx = ModernGL.create_standalone_context()
+    ctx = moderngl.create_standalone_context()
 
     if args.info:
         print(json.dumps(ctx.info, sort_keys=True, indent=4))
@@ -29,7 +29,7 @@ def main(argv=None):
         repo = os.path.isfile(os.path.join(os.path.dirname(__file__), 'README.md'))
         install = '(git repository)' if repo else '(installed)'
 
-        print('ModernGL:', ModernGL.__version__, install)
+        print('moderngl:', moderngl.__version__, install)
         print('Vendor:', ctx.info['GL_VENDOR'])
         print('Renderer:', ctx.info['GL_RENDERER'])
         print('Version:', ctx.info['GL_VERSION'])
