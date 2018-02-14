f8d8ce2a28ef4edaa511801b6fec14a5efe67dfd
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-02-14 07:47:57 +0200

    fix broken test

--- tests/test_code_style.py
+++ tests/test_code_style.py
@@ -2,13 +2,13 @@ import os
 import unittest
 
 import moderngl
-import pep8
+import pycodestyle
 
 
 class TestCase(unittest.TestCase):
     def test_style(self):
         config_file = os.path.join(os.path.dirname(__file__), '..', 'tox.ini')
-        style = pep8.StyleGuide(config_file=config_file, ignore='E402')
+        style = pycodestyle.StyleGuide(config_file=config_file, ignore='E402')
         check = style.check_files([
             os.path.join(os.path.dirname(__file__), '../moderngl/__init__.py'),
             os.path.join(os.path.dirname(__file__), '../moderngl/__main__.py'),
