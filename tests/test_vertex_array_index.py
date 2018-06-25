f6c2f456fe0dba2c818c3ee23a844896e97f1ecd
Einar Forselv <eforselv@gmail.com>
2018-06-25 23:16:50 +0200

    Basic test for incorrect element size

--- tests/test_vertex_array_index.py
+++ tests/test_vertex_array_index.py
@@ -61,6 +61,33 @@ class TestCase(unittest.TestCase):
         res = np.frombuffer(vbo2.read(), dtype='f4')
         np.testing.assert_almost_equal(res, vertices * count)
 
+    def test_2(self):
+        prog = self.ctx.program(
+            vertex_shader='''
+                #version 330
+
+                in vec4 in_vert;
+                out vec4 out_vert;
+
+                void main() {
+                    out_vert = in_vert;
+                }
+            ''',
+            varyings=['out_vert']
+        )
+
+        vertices = [
+            4.0, 2.0, 7.5, 1.8,
+            3.0, 2.8, 6.0, 10.0
+        ]
+        indices = [0, 1, 0, 1, 0, 1, 0, 1.0, 1.0]
+
+        vbo1 = self.ctx.buffer(np.array(vertices, dtype='f4').tobytes())
+        index_u4 = self.ctx.buffer(np.array(indices, dtype='u4').tobytes())
+
+        with self.assertRaises(moderngl.Error):
+            self.ctx.simple_vertex_array(prog, vbo1, 'in_vert', index_buffer=index_u4, index_element_size=0)
+
 
 if __name__ == '__main__':
     unittest.main()
