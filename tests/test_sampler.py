fce4a6be3457d49ff6d0f5ac2f64cac59a6e9d1b
Einar Forselv <eforselv@gmail.com>
2018-07-17 18:16:14 +0200

    Basic sampler test

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -1,8 +1,23 @@
-import ctypes
-import moderngl_debugger
-import moderngl as mgl
+import unittest
 
-ctx = mgl.create_standalone_context()
-sampler = ctx.sampler()
-print(sampler.foo())
-print(sampler.bar)
+import moderngl
+
+from common import get_context
+
+
+class TestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ctx = get_context()
+
+    def test_attributes(self):
+        sampler = self.ctx.sampler()
+
+        # Default values
+        assert sampler.anisotropy == 1.0
+        assert sampler.repeat_x is True
+        assert sampler.repeat_y is True
+        assert sampler.filter == (moderngl.LINEAR, moderngl.LINEAR)
+        print(sampler.compare_func)
+        assert sampler.compare_func == '?'
