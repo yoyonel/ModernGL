0143fc163fda06cbcf93e1a9623fd1356a97a46f
Einar Forselv <eforselv@gmail.com>
2018-07-17 19:02:49 +0200

    Test setting sampler attributes

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -19,5 +19,17 @@ class TestCase(unittest.TestCase):
         assert sampler.repeat_x is True
         assert sampler.repeat_y is True
         assert sampler.filter == (moderngl.LINEAR, moderngl.LINEAR)
-        print(sampler.compare_func)
         assert sampler.compare_func == '?'
+
+        # Change values
+        sampler.anisotropy = 2.0
+        sampler.repeat_x = False
+        sampler.repeat_y = False
+        sampler.filter = (moderngl.NEAREST_MIPMAP_NEAREST, moderngl.NEAREST)
+        sampler.compare_func = "<="
+
+        assert sampler.anisotropy == 2.0
+        assert sampler.repeat_x is False
+        assert sampler.repeat_y is False
+        assert sampler.filter == (moderngl.NEAREST_MIPMAP_NEAREST, moderngl.NEAREST)
+        assert sampler.compare_func == "<="
