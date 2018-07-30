ced1550e5ee51c022ecb4ec366e7640834adea47
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-07-30 20:55:10 +0300

    new builds

--- extras/build.bat
+++ extras/build.bat
@@ -6,9 +6,11 @@ call python setup.py sdist
 
 call python3.5-32 setup.py bdist_wheel
 call python3.6-32 setup.py bdist_wheel
+call python3.7-32 setup.py bdist_wheel
 
 call python3.5 setup.py bdist_wheel
 call python3.6 setup.py bdist_wheel
+call python3.7 setup.py bdist_wheel
 
 git archive HEAD --format=tar | ssh -i %USERPROFILE%\.ssh\id_rsa root@linux32.builder "rm -rf build && mkdir build && cd build && tar -x && python3.5 setup.py bdist_wheel && python3.6 setup.py bdist_wheel"
 scp -i %USERPROFILE%\.ssh\id_rsa root@linux32.builder:build/dist/*.whl dist/
