f32909404c0ea86db9d964cde31c413acf11fc81
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-08-01 08:48:24 +0300

    bump to version 5.4.2

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ classifiers = [
 
 setup(
     name='moderngl',
-    version='5.4.1',
+    version='5.4.2',
     description=short_description,
     long_description=long_description,
     long_description_content_type='text/markdown',
