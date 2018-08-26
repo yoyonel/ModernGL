5f80b37a92c4c1872158bce8d0e60e365e1bf733
ATTY Lionel <yoyonel@hotmail.com>
2018-08-26 22:23:49 +0200

    Test Depth Samplers: update/refactor tests. Add some requirements (for tests).

--- examples/conftest.py
+++ examples/conftest.py
@@ -4,6 +4,12 @@ import numpy as np
 import pytest
 
 
+# https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+# https://en.wikipedia.org/wiki/Machine_epsilon
+# binary32	single precision	float	2	24 (one bit is implicit)	2−24 ≈ 5.96e-08	2−23 ≈ 1.19e-07
+EPSILON_IEEE_754 = 5.96e-08
+
+
 @pytest.fixture(scope="session", autouse=True)
 def standalone_context():
     return mgl.create_standalone_context()
@@ -73,3 +79,52 @@ def vbo_triangle(standalone_context):
 def vbo_quad(standalone_context):
     canvas_fs_quad = np.array([0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]).astype('f4')
     return standalone_context.buffer(canvas_fs_quad.tobytes())
+
+
+@pytest.fixture
+def prog_shadow_test(standalone_context):
+    prog = standalone_context.program(
+        vertex_shader='''
+                #version 330
+
+                in vec2 in_vert;
+                out vec2 uv;
+
+                void main() {
+                    uv = in_vert;
+                    gl_Position = vec4(in_vert * 2.0 - 1.0, 0.0, 1.0);
+                }
+        ''',
+        fragment_shader='''
+                #version 330
+    
+                uniform sampler2DShadow depth;
+    
+                uniform vec2 u_shadow_coord_offset;
+    
+                uniform float u_depth_bias;
+                uniform float u_depth_triangle;
+    
+                in vec2 uv;
+                out float fragColor;
+    
+                void main() {
+                    // Compute depth caster
+                    float depth_shadow_caster = u_depth_triangle + u_depth_bias;
+                    // Compute shadow coordinates + depth of caster
+                    vec3 shadow_coord = vec3(uv, depth_shadow_caster);
+                    shadow_coord += vec3(u_shadow_coord_offset, 0.0);
+                    // Compute visibility (shadow test: receiver (potential) vs caster)
+                    float visibility = texture(depth, shadow_coord);
+                    // Output the visibility
+                    fragColor = visibility;
+                }
+            ''',
+    )
+    #
+    prog['u_depth_bias'].value = EPSILON_IEEE_754
+    depth_triangle_in_obj_space = 0
+    depth_triangle_in_view_space = (depth_triangle_in_obj_space + 1)*0.5
+    prog['u_depth_triangle'].value = depth_triangle_in_view_space
+    #
+    return prog
--- examples/test_depth_samplers.py
+++ examples/test_depth_samplers.py
@@ -7,7 +7,7 @@ import moderngl as mgl
 from scipy import ndimage
 
 
-def _raster_triangle(ctx, prog, vbo, size=(4, 4)):
+def _build_fbo_with_rasterised_triangle(ctx, prog, vbo, size=(4, 4), depth_clear=1.0):
     """
 
     :param ctx:
@@ -19,7 +19,7 @@ def _raster_triangle(ctx, prog, vbo, size=(4, 4)):
     tex_depth = ctx.depth_texture(size)  # implicit -> dtype='f4', components=1
     fbo_depth = ctx.framebuffer(depth_attachment=tex_depth)
     fbo_depth.use()
-    fbo_depth.clear(depth=1.0)
+    fbo_depth.clear(depth=depth_clear)
 
     # vertex array object of triangle with depth pass prog
     vao_triangle = ctx.simple_vertex_array(prog, vbo, 'in_vert')
@@ -43,7 +43,7 @@ def test_depth_sampler(standalone_context, prog_render_depth_pass, prog_draw_dep
     ctx.enable(mgl.DEPTH_TEST)
 
     size = (16, 16)
-    fbo_depth, tex_depth = _raster_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
+    fbo_depth, tex_depth = _build_fbo_with_rasterised_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
 
     # It should have some different values where the triangle lies.
     depth_from_dbo = np.frombuffer(tex_depth.read(), dtype=np.dtype('f4')).reshape(size[::-1])
@@ -76,11 +76,18 @@ def test_depth_sampler(standalone_context, prog_render_depth_pass, prog_draw_dep
     np.testing.assert_array_almost_equal(data, np_triangle_raster)
 
 
-def test_sampler_shadow(standalone_context, prog_render_depth_pass, vbo_triangle, vbo_quad):
+def test_sampler_shadow(
+        standalone_context,
+        prog_render_depth_pass,
+        prog_shadow_test,
+        vbo_triangle,
+        vbo_quad
+):
     """
 
     :param standalone_context:
     :param prog_render_depth_pass:
+    :param prog_shadow_test:
     :param vbo_triangle:
     :param vbo_quad:
     :return:
@@ -88,44 +95,14 @@ def test_sampler_shadow(standalone_context, prog_render_depth_pass, vbo_triangle
     ctx = standalone_context
     ctx.enable(mgl.DEPTH_TEST)
 
-    size = (4, 4)
-    fbo_depth, tex_depth = _raster_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
+    size = (3, 3)
+    fbo_depth, tex_depth = _build_fbo_with_rasterised_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
 
-    prog_for_shadow = ctx.program(
-        vertex_shader='''
-                    #version 330
+    # No offset - fetch the center pixel (NEAREST)
+    prog_shadow_test['u_shadow_coord_offset'].value = (0,) * 2
 
-                    in vec2 in_vert;
-                    out vec2 uv;
-
-                    void main() {
-                        uv = in_vert;
-                        gl_Position = vec4(in_vert * 2.0 - 1.0, 0.0, 1.0);
-                    }
-                ''',
-        fragment_shader='''
-                    #version 330
-
-                    uniform sampler2DShadow depth;
-
-                    uniform float depth_bias;
-                    uniform float depth_triangle;
-                     
-                    in vec2 uv;
-                    out float fragColor;
-
-                    void main() {
-                        float depth_shadow_caster = depth_triangle + depth_bias;
-                        vec3 shadow_coord = vec3(uv, depth_shadow_caster);
-                        float visibility = texture(depth, shadow_coord);
-                        fragColor = visibility;
-                    }
-                ''',
-    )
-    prog_for_shadow['depth_bias'].value = 0.00001
-    prog_for_shadow['depth_triangle'].value = (0 + 1) * 0.5
     # https://www.khronos.org/opengl/wiki/Sampler_(GLSL)#Shadow_samplers
-    vao_fs_shadow = ctx.simple_vertex_array(prog_for_shadow, vbo_quad, 'in_vert')
+    vao_fs_shadow = ctx.simple_vertex_array(prog_shadow_test, vbo_quad, 'in_vert')
 
     fbo_draw_depth = ctx.framebuffer(color_attachments=[ctx.renderbuffer(size, components=1, dtype='f4')])
     fbo_draw_depth.use()
@@ -149,11 +126,18 @@ def test_sampler_shadow(standalone_context, prog_render_depth_pass, vbo_triangle
     np.testing.assert_array_almost_equal(data, np_triangle_raster_visbility)
 
 
-def test_sampler_shadow_with_bilinear_interpolation(standalone_context, prog_render_depth_pass, vbo_triangle, vbo_quad):
+def test_sampler_shadow_with_bilinear_interpolation(
+        standalone_context,
+        prog_render_depth_pass,
+        prog_shadow_test,
+        vbo_triangle,
+        vbo_quad
+):
     """
 
     :param standalone_context:
     :param prog_render_depth_pass:
+    :param prog_shadow_test:
     :param vbo_triangle:
     :param vbo_quad:
     :return:
@@ -161,42 +145,15 @@ def test_sampler_shadow_with_bilinear_interpolation(standalone_context, prog_ren
     ctx = standalone_context
     ctx.enable(mgl.DEPTH_TEST)
 
-    size = (8, 8)
-    fbo_depth, tex_depth = _raster_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
+    size = (7, 7)
 
-    prog_for_shadow = ctx.program(
-        vertex_shader='''
-                    #version 330
+    fbo_depth, tex_depth = _build_fbo_with_rasterised_triangle(ctx, prog_render_depth_pass, vbo_triangle, size)
 
-                    in vec2 in_vert;
-                    out vec2 uv;
+    half_pixel_size = (1.0/size[0]*0.5, 1.0/size[1]*0.5)
+    prog_shadow_test['u_shadow_coord_offset'].value = tuple(half_pixel_size)
 
-                    void main() {
-                        uv = in_vert;
-                        gl_Position = vec4(in_vert * 2.0 - 1.0, 0.0, 1.0);
-                    }
-                ''',
-        fragment_shader='''
-                    #version 330
-
-                    uniform sampler2DShadow depth;
-                    
-                    uniform vec2 half_pixel;
-                    
-                    in vec2 uv;
-                    out float fragColor;
-
-                    void main() {
-                        vec3 shadow_coord = vec3(uv, 0.50 + 0.01);
-                        shadow_coord += vec3(half_pixel, 0.0);
-                        float visibility = texture(depth, shadow_coord);
-                        fragColor = visibility;
-                    }
-                ''',
-    )
-    prog_for_shadow['half_pixel'].value = (1.0/float(size[0]) * 0.5, 1.0/float(size[1]) * 0.5)
     # https://www.khronos.org/opengl/wiki/Sampler_(GLSL)#Shadow_samplers
-    vao_fs_shadow = ctx.simple_vertex_array(prog_for_shadow, vbo_quad, 'in_vert')
+    vao_fs_shadow = ctx.simple_vertex_array(prog_shadow_test, vbo_quad, 'in_vert')
 
     fbo_draw_depth = ctx.framebuffer(color_attachments=[ctx.renderbuffer(size, components=1, dtype='f4')])
     fbo_draw_depth.use()
