2d227bd3bbf1348127c184fc0e5753154e76a6b6
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2018-12-11 02:15:50 +0100

    Update pygame example
    it even had syntax errors

--- examples/old-examples/pygame/01_hello_world.py
+++ examples/old-examples/pygame/01_hello_world.py
@@ -1,34 +1,35 @@
 import struct
 
-import ModernGL
+import moderngl
 import pygame
 from pygame.locals import DOUBLEBUF, OPENGL
 
 pygame.init()
 pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
 
-ctx = ModernGL.create_context()
+ctx = moderngl.create_context()
 
-vert = ctx.vertex_shader('''
-    #version 330
+prog = ctx.program(
+        vertex_shader='''
+    #version 310 es
     in vec2 vert;
     void main() {
         gl_Position = vec4(vert, 0.0, 1.0);
     }
-''')
-
-frag = ctx.fragment_shader('''
-    #version 330
+''',
+        fragment_shader='''
+    #version 310 es
+    precision mediump float;
     out vec4 color;
+
     void main() {
-        color = vec4(0.30, 0.50, 1.00, 1.0);
+        color = vec4(0.3, 0.5, 1.00, 1.0);
     }
 ''')
 
-prog = ctx.program(vert, frag])
 
 vbo = ctx.buffer(struct.pack('6f', 0.0, 0.8, -0.6, -0.8, 0.6, -0.8))
-vao = ctx.simple_vertex_array(prog, vbo, ['vert'])
+vao = ctx.simple_vertex_array(prog, vbo, 'vert')
 
 running = True
 while running:
@@ -40,4 +41,4 @@ while running:
     vao.render()
 
     pygame.display.flip()
-    pygame.time.wait(10)
+    pygame.time.wait(6)
