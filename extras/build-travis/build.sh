71bb2dd13cd98ed53d34491fb73e94ab5546f34d
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-01-20 01:56:54 +0200

    fix build args

--- extras/build-travis/build.sh
+++ extras/build-travis/build.sh
@@ -2,7 +2,7 @@ set -e -x
 cd /io
 
 for PYBIN in /opt/python/cp3*/bin; do
-    "${PYBIN}/python" setup.py bdist_wheel -w dist
+    "${PYBIN}/python" setup.py bdist_wheel
 done
 
 for whl in dist/*.whl; do
