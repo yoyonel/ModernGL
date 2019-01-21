23637c4efdde21dd6f0fdba02e302a28a985aea6
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-01-21 19:51:17 +0200

    fix memset

--- moderngl/experimental/mgl/internal/python.hpp
+++ moderngl/experimental/mgl/internal/python.hpp
@@ -17,7 +17,7 @@
  * The allocated memory is initialized to zero.
  * Slots can be set using the SLOT(...) macro.
  */
-inline PyObject * _new_object(PyTypeObject * type) {
+inline PyObject * _new_object(PyTypeObject * type, int size) {
     PyObject * res = 0;
     Py_INCREF(type);
     if (type->tp_flags & Py_TPFLAGS_HAVE_GC) {
@@ -26,11 +26,11 @@ inline PyObject * _new_object(PyTypeObject * type) {
         res = PyObject_New(PyObject, type);
     }
     // PyObject_GC_Track(wrapper);
-    memset((char *)res + sizeof(PyObject), 0, type->tp_basicsize - sizeof(PyObject));
+    memset((char *)res + sizeof(PyObject), 0, size - sizeof(PyObject));
     return res;
 }
 
-#define new_object(type, typeobj) (type *)_new_object(typeobj)
+#define new_object(type, typeobj) (type *)_new_object(typeobj, sizeof(type))
 #define call_function(function, ...) PyObject_CallFunctionObjArgs(function, __VA_ARGS__, (void *)0)
 
 inline void replace_object(PyObject *& src, PyObject * dst) {
