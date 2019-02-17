e48d0276f5efc4d78f214b5d2a48c7e49d3b54d7
einarf <eforselv@gmail.com>
2019-02-17 15:13:55 +0100

    Auto-locate existing window packages

--- examples/window/__init__.py
+++ examples/window/__init__.py
@@ -1,23 +1,31 @@
 import argparse
-from importlib import import_module
 import sys
-
+from importlib import import_module
+from pathlib import Path
 
 from .base import Example, BaseWindow
 
 
 def run_example(example_cls: Example, args=None):
-    """Run an example entering a blocking main loop"""
+    """
+    Run an example entering a blocking main loop
+
+    Args:
+        example_cls: The exmaple class to render
+        args: Override sys.args
+    """
     values = parse_args(args)
     window_cls = get_window_cls(values.window)
+
     window = window_cls(
         title=example_cls.title,
         size=example_cls.window_size,
+        fullscreen=values.fullscreen,
         resizable=example_cls.resizable,
         gl_version=example_cls.gl_version,
         aspect_ratio=example_cls.aspect_ratio,
-        **vars(values)
     )
+
     window.example = example_cls(ctx=window.ctx, wnd=window)
 
     while not window.is_closing:
@@ -27,7 +35,7 @@ def run_example(example_cls: Example, args=None):
     window.destroy()
 
 
-def get_window_cls(window) -> BaseWindow:
+def get_window_cls(window: str) -> BaseWindow:
     """Attept to obtain the configured window class"""
     return import_string('window.{}.window.Window'.format(window))
 
@@ -35,21 +43,40 @@ def get_window_cls(window) -> BaseWindow:
 def parse_args(args=None):
     """Parse arguments from sys.argv"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--window", default="glfw", help="Name for the window module to use")
+
+    parser.add_argument(
+        '-w', '--window',
+        default="glfw",
+        choices=find_window_classes(),
+        help='Name for the window type to use',
+    )
+    parser.add_argument(
+        '-fs', '--fullscreen',
+        action="store_true",
+        help='Open the window in fullscreen mode',
+    )
+
     return parser.parse_args(args or sys.argv[1:])
 
 
 def find_window_classes():
-    """Find existing window classes"""
-    pass
+    """Find available window packages"""
+    return [
+        path.parts[-1] for path in Path(__file__).parent.iterdir()
+        if path.is_dir() and not path.parts[-1].startswith('__')
+    ]
 
 
 def import_string(dotted_path):
     """
     Import a dotted module path and return the attribute/class designated by the
     last name in the path. Raise ImportError if the import failed.
-    :param dotted_path: The path to attempt importing
-    :return: The object
+    
+    Args:
+        dotted_path: The path to attempt importing
+
+    Returns:
+        Imported class/attribute
     """
     try:
         module_path, class_name = dotted_path.rsplit('.', 1)
