ecbabe5e99f988449ee85f19d137b095a7008cb6
einarf <eforselv@gmail.com>
2019-02-19 22:36:01 +0100

    Example batch #2

--- examples/julia_fractal.py
+++ examples/julia_fractal.py
@@ -2,12 +2,14 @@ import moderngl
 
 import numpy as np
 
-from example_window import Example, run_example
+from window import Example, run_example
 
 
 class Fractal(Example):
-    def __init__(self):
-        self.ctx = moderngl.create_context()
+    title = "Julia Fractal"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
         self.prog = self.ctx.program(
             vertex_shader='''
@@ -63,8 +65,7 @@ class Fractal(Example):
         self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
 
-    def render(self):
-        self.ctx.viewport = self.wnd.viewport
+    def render(self, time, frame_time):
         self.ctx.clear(1.0, 1.0, 1.0)
 
         self.center.value = (0.49, 0.32)
--- examples/julia_set.py
+++ examples/julia_set.py
@@ -4,7 +4,7 @@ import moderngl
 import numpy as np
 from PIL import Image
 
-from example_window import Example, run_example
+from window import Example, run_example
 
 
 def local(*path):
@@ -12,8 +12,10 @@ def local(*path):
 
 
 class Fractal(Example):
-    def __init__(self):
-        self.ctx = moderngl.create_context()
+    title = "Julia Set"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
         self.prog = self.ctx.program(
             vertex_shader='''
@@ -72,8 +74,7 @@ class Fractal(Example):
         self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
 
-    def render(self):
-        self.ctx.viewport = self.wnd.viewport
+    def render(self, time, frame_time):
         self.ctx.clear(1.0, 1.0, 1.0)
 
         self.seed.value = (-0.8, 0.156)
