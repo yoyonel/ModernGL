9127df7cdaed1b50fc6aacd4244db3505aab3157
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-05-01 07:57:45 +0300

    improve from_dtype

--- moderngl/mgl/internal/data_type.cpp
+++ moderngl/mgl/internal/data_type.cpp
@@ -3,7 +3,7 @@
 #include "opengl/opengl.hpp"
 
 MGLDataType f1 = {
-    0,
+    NULL,
     {0, GL_RED, GL_RG, GL_RGB, GL_RGBA},
     {0, GL_R8, GL_RG8, GL_RGB8, GL_RGBA8},
     GL_UNSIGNED_BYTE,
@@ -12,7 +12,7 @@ MGLDataType f1 = {
 };
 
 MGLDataType f2 = {
-    0,
+    NULL,
     {0, GL_RED, GL_RG, GL_RGB, GL_RGBA},
     {0, GL_R16F, GL_RG16F, GL_RGB16F, GL_RGBA16F},
     GL_HALF_FLOAT,
@@ -21,7 +21,7 @@ MGLDataType f2 = {
 };
 
 MGLDataType f4 = {
-    0,
+    NULL,
     {0, GL_RED, GL_RG, GL_RGB, GL_RGBA},
     {0, GL_R32F, GL_RG32F, GL_RGB32F, GL_RGBA32F},
     GL_FLOAT,
@@ -30,7 +30,7 @@ MGLDataType f4 = {
 };
 
 MGLDataType u1 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R8UI, GL_RG8UI, GL_RGB8UI, GL_RGBA8UI},
     GL_UNSIGNED_BYTE,
@@ -39,7 +39,7 @@ MGLDataType u1 = {
 };
 
 MGLDataType u2 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R16UI, GL_RG16UI, GL_RGB16UI, GL_RGBA16UI},
     GL_UNSIGNED_SHORT,
@@ -48,7 +48,7 @@ MGLDataType u2 = {
 };
 
 MGLDataType u4 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R32UI, GL_RG32UI, GL_RGB32UI, GL_RGBA32UI},
     GL_UNSIGNED_INT,
@@ -57,7 +57,7 @@ MGLDataType u4 = {
 };
 
 MGLDataType i1 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R8I, GL_RG8I, GL_RGB8I, GL_RGBA8I},
     GL_BYTE,
@@ -66,7 +66,7 @@ MGLDataType i1 = {
 };
 
 MGLDataType i2 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R16I, GL_RG16I, GL_RGB16I, GL_RGBA16I},
     GL_SHORT,
@@ -75,7 +75,7 @@ MGLDataType i2 = {
 };
 
 MGLDataType i4 = {
-    0,
+    NULL,
     {0, GL_RED_INTEGER, GL_RG_INTEGER, GL_RGB_INTEGER, GL_RGBA_INTEGER},
     {0, GL_R32I, GL_RG32I, GL_RGB32I, GL_RGBA32I},
     GL_INT,
@@ -84,7 +84,7 @@ MGLDataType i4 = {
 };
 
 MGLDataType d2 = {
-    0,
+    NULL,
     {0, GL_DEPTH_COMPONENT, 0, 0, 0},
     {0, GL_DEPTH_COMPONENT16, 0, 0, 0},
     GL_HALF_FLOAT,
@@ -93,7 +93,7 @@ MGLDataType d2 = {
 };
 
 MGLDataType d3 = {
-    0,
+    NULL,
     {0, GL_DEPTH_COMPONENT, 0, 0, 0},
     {0, GL_DEPTH_COMPONENT24, 0, 0, 0},
     GL_FLOAT,
@@ -102,7 +102,7 @@ MGLDataType d3 = {
 };
 
 MGLDataType d4 = {
-    0,
+    NULL,
     {0, GL_DEPTH_COMPONENT, 0, 0, 0},
     {0, GL_DEPTH_COMPONENT32, 0, 0, 0},
     GL_FLOAT,
@@ -111,51 +111,45 @@ MGLDataType d4 = {
 };
 
 MGLDataType * from_dtype(PyObject * dtype) {
-    if (PyUnicode_Check(dtype)) {
+    if (PyUnicode_Check(dtype) && PyUnicode_GET_LENGTH(dtype) == 2) {
         short * code = (short *)PyUnicode_AsUTF8(dtype);
-        if (PyUnicode_GET_LENGTH(dtype) == 2) {
-            switch (*code) {
-                case 'f' | '1' << 8:
-                    return &f1;
+        switch (*code) {
+            case 'f' | '1' << 8:
+                return &f1;
 
-                case 'f' | '2' << 8:
-                    return &f2;
+            case 'f' | '2' << 8:
+                return &f2;
 
-                case 'f' | '4' << 8:
-                    return &f4;
+            case 'f' | '4' << 8:
+                return &f4;
 
-                case 'u' | '1' << 8:
-                    return &u1;
+            case 'u' | '1' << 8:
+                return &u1;
 
-                case 'u' | '2' << 8:
-                    return &u2;
+            case 'u' | '2' << 8:
+                return &u2;
 
-                case 'u' | '4' << 8:
-                    return &u4;
+            case 'u' | '4' << 8:
+                return &u4;
 
-                case 'i' | '1' << 8:
-                    return &i1;
+            case 'i' | '1' << 8:
+                return &i1;
 
-                case 'i' | '2' << 8:
-                    return &i2;
+            case 'i' | '2' << 8:
+                return &i2;
 
-                case 'i' | '4' << 8:
-                    return &i4;
+            case 'i' | '4' << 8:
+                return &i4;
 
-                case 'd' | '2' << 8:
-                    return &d2;
+            case 'd' | '2' << 8:
+                return &d2;
 
-                case 'd' | '3' << 8:
-                    return &d3;
+            case 'd' | '3' << 8:
+                return &d3;
 
-                case 'd' | '4' << 8:
-                    return &d4;
-
-                default:
-                    return 0;
-            }
+            case 'd' | '4' << 8:
+                return &d4;
         }
-        return 0;
     }
-    return 0;
+    return NULL;
 }
