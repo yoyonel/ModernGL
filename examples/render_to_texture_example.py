1a2fc73fe0ec3cff1d94fd4526aeb2ba12b589a5
einarf <eforselv@gmail.com>
2019-08-04 18:37:37 +0200

    render_to_texture: Greatly simplify by extending ColorsAndTextures

--- examples/render_to_texture_example.py
+++ examples/render_to_texture_example.py
@@ -1,88 +1,23 @@
 import os
 
 import numpy as np
-from objloader import Obj
-from PIL import Image
 from pyrr import Matrix44
 
-import data
 import moderngl
 from ported._example import Example
+from basic_colors_and_texture import ColorsAndTexture
 
 
-class RenderToTexture(Example):
+class RenderToTexture(ColorsAndTexture):
     title = "Render to Texture"
     gl_version = (3, 3)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.prog = self.ctx.program(
-            vertex_shader='''
-                #version 330
-
-                uniform mat4 Mvp;
-
-                in vec3 in_vert;
-                in vec3 in_norm;
-                in vec2 in_text;
-
-                out vec3 v_vert;
-                out vec3 v_norm;
-                out vec2 v_text;
-
-                void main() {
-                    gl_Position = Mvp * vec4(in_vert, 1.0);
-                    v_vert = in_vert;
-                    v_norm = in_norm;
-                    v_text = in_text;
-                }
-            ''',
-            fragment_shader='''
-                #version 330
-
-                uniform vec3 Light;
-                uniform vec3 Color;
-                uniform bool UseTexture;
-                uniform sampler2D Texture;
-
-                in vec3 v_vert;
-                in vec3 v_norm;
-                in vec2 v_text;
-
-                out vec4 f_color;
-
-                void main() {
-                    float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.8 + 0.2;
-                    if (UseTexture) {
-                        f_color = vec4(texture(Texture, v_text).rgb * lum, 1.0);
-                    } else {
-                        f_color = vec4(Color * lum, 1.0);
-                    }
-                }
-            ''',
-        )
-
-        self.mvp = self.prog['Mvp']
-        self.light = self.prog['Light']
-        self.color = self.prog['Color']
-        self.use_texture = self.prog['UseTexture']
-
-        self.objects = {}
-
-        for name in ['ground', 'grass', 'billboard', 'billboard-holder', 'billboard-image']:
-            obj = Obj.open(data.find('scene-1-%s.obj' % name))
-            vbo = self.ctx.buffer(obj.pack('vx vy vz nx ny nz tx ty'))
-            vao = self.ctx.simple_vertex_array(self.prog, vbo, 'in_vert', 'in_norm', 'in_text')
-            self.objects[name] = vao
-
-        img = Image.open(data.find('infographic-1.jpg')).transpose(Image.FLIP_TOP_BOTTOM).convert('RGB')
-        self.texture1 = self.ctx.texture(img.size, 3, img.tobytes())
-        self.texture1.build_mipmaps()
-
-        self.texture2 = self.ctx.texture(self.wnd.size, 3)
+        self.texture = self.ctx.texture(self.wnd.size, 3)
         depth_attachment = self.ctx.depth_renderbuffer(self.wnd.size)
-        self.fbo = self.ctx.framebuffer(self.texture2, depth_attachment)
+        self.fbo = self.ctx.framebuffer(self.texture, depth_attachment)
 
     def render(self, time, frame_time):
         self.ctx.clear(1.0, 1.0, 1.0)
@@ -101,36 +36,10 @@ class RenderToTexture(Example):
             if mode == 'render_to_texture':
                 self.fbo.clear(1.0, 1.0, 1.0)
                 self.fbo.use()
-
             else:
                 self.ctx.screen.use()
 
-            self.use_texture.value = False
-
-            self.light.value = (67.69, -8.14, 52.49)
-            self.mvp.write((proj * lookat * rotate).astype('f4').tobytes())
-
-            self.color.value = (0.67, 0.49, 0.29)
-            self.objects['ground'].render()
-
-            self.color.value = (0.46, 0.67, 0.29)
-            self.objects['grass'].render()
-
-            self.color.value = (1.0, 1.0, 1.0)
-            self.objects['billboard'].render()
-
-            self.color.value = (0.2, 0.2, 0.2)
-            self.objects['billboard-holder'].render()
-
-            self.use_texture.value = True
-
-            if mode == 'render_to_texture':
-                self.texture1.use()
-
-            else:
-                self.texture2.use()
-
-            self.objects['billboard-image'].render()
+            super().render(time, frame_time)
 
 
 if __name__ == '__main__':
