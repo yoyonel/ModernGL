29d85e36acc4670888a2ade97071007a5131a8c9
Einar Forselv <eforselv@gmail.com>
2020-01-22 23:48:45 +0100

    docs: Add basic branding

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.ico"
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
