cd9b3aa9ea3e1df85a2f5bc0dcf55d2ac446b7ad
Einar Forselv <eforselv@gmail.com>
2020-01-26 19:50:53 +0100

    tests: context sharing

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import moderngl
+import numpy
 import platform
 
 
@@ -41,5 +42,34 @@ class ContextTests(TestCase):
         ctx2.release()
 
     def test_share(self):
+        """Create resources with shared context"""
         if platform.system().lower() == "darwin":
             self.skipTest('Context sharing not supported on darwin')
+
+        data1 = numpy.array([1, 2, 3, 4], dtype='u1')
+        data2 = numpy.array([4, 3, 2, 1], dtype='u1')
+
+        ctx1 = moderngl.create_context(standalone=True)
+        ctx2 = moderngl.create_context(standalone=True, share=True)
+
+        with ctx1 as ctx:
+            b1 = ctx.buffer(data=data1)
+
+        with ctx2 as ctx:
+            b2 = ctx.buffer(data=data2)
+
+        # Because the resources are shared the name should increment
+        self.assertEqual(b1.glo, 1)
+        self.assertEqual(b2.glo, 2)
+
+        # Ensure we can read the same buffer data in both contexts
+        with ctx1:
+            self.assertEqual(b1.read(), b'\x01\x02\x03\x04')
+            self.assertEqual(b2.read(), b'\x04\x03\x02\x01')
+
+        with ctx2:
+            self.assertEqual(b1.read(), b'\x01\x02\x03\x04')
+            self.assertEqual(b2.read(), b'\x04\x03\x02\x01')
+
+        ctx1.release()
+        ctx2.release()
