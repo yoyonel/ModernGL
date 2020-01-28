0c8efb1e04269ed3296ea7d67e985003946b734d
Einar Forselv <eforselv@gmail.com>
2020-01-28 02:14:59 +0100

    Examples: Remove next directory from master. It's confusing to users

--- examples/next/data.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def find(name):
-    return os.path.normpath(os.path.abspath(os.path.join(__file__, '../../data', name)))
-
-
-def open(name, mode):
-    return open(find(name), mode)
