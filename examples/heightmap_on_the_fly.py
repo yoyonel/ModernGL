b045f5451a42d227a32c7d5935f74aed0c571893
Einar Forselv <eforselv@gmail.com>
2020-07-06 18:34:31 +0200

    heightmap: vertex normals from heights

--- examples/heightmap_on_the_fly.py
+++ examples/heightmap_on_the_fly.py
@@ -4,7 +4,7 @@ pre-generated geometry.
 """
 
 import numpy as np
-from pyrr import Matrix44
+from pyrr import Matrix44, Matrix33
 
 import moderngl
 from ported._example import Example
@@ -37,14 +37,31 @@ class HeightmapOnTheFly(Example):
 
             uniform mat4 projection;
             uniform mat4 modelview;
+            uniform mat3 normal_matrix;
             uniform int dim;
             uniform float terrain_size;
 
             out vec2 g_uv;
-            out vec3 g_pos;
+            // out vec3 g_pos;
+            out vec3 normal;
 
             layout(points) in;
             layout(triangle_strip, max_vertices = 4) out;
+
+            const float scale = 0.5;
+            const float height = -0.15;
+
+            float calculateHeight(float h) {
+                return h * scale + height;
+            }
+
+            vec3 calculateNormal(vec2 uv, float step, float size) {
+                float hl = calculateHeight(texture(heightmap, uv + vec2(-step, 0.0)).r);
+                float hr = calculateHeight(texture(heightmap, uv + vec2(step, 0.0)).r);
+                float hu = calculateHeight(texture(heightmap, uv + vec2(0.0, step)).r);
+                float hd = calculateHeight(texture(heightmap, uv + vec2(0.0, -step)).r);
+                return normalize(vec3(hl - hr, hd - hu, size));
+            }
 
             void main() {
                 // width and height of a quad
@@ -59,48 +76,48 @@ class HeightmapOnTheFly(Example):
                 mat4 mvp = projection * modelview;
 
                 // Read heights for each corner
-                const float scale = 0.5;
-                const float height = -0.15;
-
                 vec2 uv1 = uv + vec2(0.0, uv_step);
-                float h1 = texture(heightmap, uv1).r * scale + height;
+                float h1 = calculateHeight(texture(heightmap, uv1).r);
 
                 vec2 uv2 = uv;
-                float h2 = texture(heightmap, uv2).r * scale + height;
+                float h2 = calculateHeight(texture(heightmap, uv2).r);
 
                 vec2 uv3 = uv + vec2(uv_step, uv_step);
-                float h3 = texture(heightmap, uv3).r * scale + height;
+                float h3 = calculateHeight(texture(heightmap, uv3).r);
 
                 vec2 uv4 = uv + vec2(uv_step, 0.0);
-                float h4 = texture(heightmap, uv4).r * scale + height;
-
+                float h4 = calculateHeight(texture(heightmap, uv4).r);
 
                 // Upper left
                 vec4 pos1 = vec4(pos + vec2(0.0, size), h1, 1.0);
                 gl_Position = mvp * pos1;
                 g_uv = uv1;
-                g_pos = (modelview * pos1).xyz;
+                normal = normal_matrix * calculateNormal(uv1, uv_step, size);
+                // g_pos = (modelview * pos1).xyz;
                 EmitVertex();
 
                 // Lower left
                 vec4 pos2 = vec4(pos, h2, 1.0);
                 gl_Position = mvp * pos2;
                 g_uv = uv2;
-                g_pos = (modelview * pos2).xyz;
+                normal = normal_matrix * calculateNormal(uv2, uv_step, size);
+                // g_pos = (modelview * pos2).xyz;
                 EmitVertex();
 
                 // Upper right
                 vec4 pos3 = vec4(pos + vec2(size, size), h3, 1.0);
                 gl_Position = mvp * pos3;
                 g_uv = uv3;
-                g_pos = (modelview * pos3).xyz;
+                normal = normal_matrix * calculateNormal(uv3, uv_step, size);
+                // g_pos = (modelview * pos3).xyz;
                 EmitVertex();
 
                 // Lower right
                 vec4 pos4 = vec4(pos + vec2(size, 0.0), h4, 1.0);
                 gl_Position = mvp * pos4;
                 g_uv = uv4;
-                g_pos = (modelview * pos4).xyz;
+                normal = normal_matrix * calculateNormal(uv4, uv_step, size);
+                // g_pos = (modelview * pos4).xyz;
                 EmitVertex();
 
                 EndPrimitive();
@@ -112,15 +129,16 @@ class HeightmapOnTheFly(Example):
                 uniform sampler2D heightmap;
                 out vec4 fragColor;
                 in vec2 g_uv;
-                in vec3 g_pos;
+                // in vec3 g_pos;
+                in vec3 normal;
 
                 void main() {
-                    vec3 normal = normalize(cross(dFdx(g_pos), dFdy(g_pos)));
+                    // vec3 normal = normalize(cross(dFdx(g_pos), dFdy(g_pos)));
                     float l = abs(dot(vec3(0, 0, 1), normal));
 
                     // fragColor = vec4(vec3(texture(heightmap, g_uv).r) * l, 1.0);
-                    // fragColor = vec4(normal, 1.0) + vec4(1.0);
-                    fragColor = vec4(vec3(0.75) * l, 1.0);
+                    // fragColor = vec4(normal * l, 1.0);
+                    fragColor = vec4(vec3(1.0) * l, 1.0);
                 }
             """,
         )
@@ -146,8 +164,10 @@ class HeightmapOnTheFly(Example):
             (0.0, 0.0, 1.0),
             dtype='f4',
         )
+        normal_matrix = Matrix33.from_matrix44(lookat).inverse.transpose()
 
         self.prog['modelview'].write(lookat)
+        self.prog['normal_matrix'].write(normal_matrix.astype('f4').tobytes())
         self.heightmap.use(0)
         self.vao.render(moderngl.POINTS, vertices=(self.dim - 1) ** 2)
 
